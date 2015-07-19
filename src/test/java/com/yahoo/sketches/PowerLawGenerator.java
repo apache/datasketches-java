67f1779f63abe03af7ee8cee47990f3e98486a28
Lee Rhodes <lrhodes@yahoo-inc.com>
2015-07-19 11:58:44 -0700

    Add PowerLawGenerator and code examples into test package

---
+++ src/test/java/com/yahoo/sketches/PowerLawGenerator.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+package com.yahoo.sketches;
+
+import static java.lang.Math.*;
+
+/**
+ * A simple tool to generate (x,y) pairs of points that follow a power-law relationship.
+ * This class provides both a convenient dynamic class with simplified get methods or the 
+ * equalivant static calls with full arguments. The basic static methods can also be used to 
+ * generate equally spaced points for a single axis.
+ * 
+ * <p>The equal spacing is created using "generating indices" that are first obtained from 
+ * these methods and then used to generate the x-coordiantes or full (x,y) Pairs.  Please refer
+ * to example code in the associated test class.
+ * </p>
+ * 
+ * @author Lee Rhodes
+ */
+public class PowerLawGenerator {
+  
+  /**
+   * A simple x,y coordinate pair of values
+   */
+  public static class Pair {
+    public double x;
+    public double y;
+    public Pair (double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  //Inputs
+  private int ppxb_;
+  private double xLogBase_;
+  private Pair start_;
+  private Pair end_;
+  
+  //Derived
+  private int iStart_;
+  private int iEnd_;
+  private int numIdxs_;
+  private double slope_;
+  private int delta_;
+  
+  /**
+   * Creates a convenient dynamic class used for generating (x,y) Pairs for a power-law relation. 
+   * This creates the intermediate mathematical values from the given parameters will allow
+   * simplified gets with few or no parameters.
+   * 
+   * @param xLogBase The logarithmic base for the x-coordinate
+   * @param ptsPerXBase The desired resolution specified in number of equally spaced points per 
+   * power of the x logarithmic base.  For example, if x is log-base2 specifying 4 would
+   * result in 4 equally spaced points for every power of 2 of x.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   */
+  public PowerLawGenerator(double xLogBase, int ptsPerXBase, Pair start, Pair end) {
+    xLogBase_ = xLogBase;
+    ppxb_ = ptsPerXBase;
+    start_ = start;
+    end_ = end;
+    
+    slope_ = getSlope(start_, end_);
+    iStart_ = getStartGenIndex(start_, end_, xLogBase_, ppxb_);
+    iEnd_ = getEndGenIndex(start_, end_, xLogBase_, ppxb_);
+    delta_ = getDelta(start_, end_);
+    numIdxs_ = getNumGenIndices(start_, end_, xLogBase_, ppxb_);
+  }
+  
+  /**
+   * The total number of generating indices available given the start and end Pairs.
+   * @return the number of generating indices.
+   */
+  public int getNumGenIndices() {
+    return numIdxs_;
+  }
+  
+  /**
+   * Returns the generating index delta value. If start.x &lt; end.x then this returns 1, else -1.
+   * @return the generating index delta value
+   */
+  public int getDelta() {
+    return delta_;
+  }
+  
+  /**
+   * Returns the power-law slope derived from the start and end Pairs.
+   * @return the power-law slope derived from the start and end Pairs.
+   */
+  public double getSlope() {
+    return slope_;
+  }
+  
+  /**
+   * Returns the starting generating index used to generate the start Point.
+   * @return the starting generating index used to obtain the start Point.
+   */
+  public int getStartGenIndex() {
+    return iStart_;
+  }
+  
+  /**
+   * Returns the ending generating index used to generate the end Point.
+   * @return the ending generating index used to obtain the end Point.
+   */
+  public int getEndGenIndex() {
+    return iEnd_;
+  }
+  
+  /**
+   * Returns a coordinate Pair based on the given generating index.
+   * @param genIndex the given generating index
+   * @return a coordinate Pair based on the given generating index.
+   */
+  public Pair getPair(int genIndex) {
+    double x = getX(genIndex, xLogBase_, ppxb_);
+    double y = getY(start_, slope_, x);
+    return new Pair(x, y);
+  }
+  
+  //Static method equivalents
+  
+  /**
+   * The total number of generating indices available given the start and end Pairs.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   * @param xLogBase The logarithmic base for the x-coordinate
+   * @param ptsPerXBase The desired resolution specified in number of equally spaced points per 
+   * power of the x logarithmic base.  For example, if x is log-base2 specifying 4 would
+   * result in 4 equally spaced points for every power of 2 of x.
+   * @return The total number of generating indices available given the start and end Pairs.
+   */
+  public static int getNumGenIndices(Pair start, Pair end, double xLogBase, int ptsPerXBase) {
+    int iStrt = getStartGenIndex(start, end, xLogBase, ptsPerXBase);
+    int iEnd =  getEndGenIndex(start, end, xLogBase, ptsPerXBase);
+    return abs(iStrt - iEnd) +1;
+  }
+  
+  /**
+   * Returns the generating index delta value. If start.x &lt; end.x then this returns 1, else -1.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   * @return the generating index delta value
+   */
+  public static int getDelta(Pair start, Pair end) {
+    boolean xIncreasing = end.x > start.x;
+    return (xIncreasing)? 1 : -1;
+  }
+  
+  /**
+   * Returns the power-law slope derived from the start and end Pairs.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   * @return the power-law slope derived from the start and end Pairs
+   */
+  public static double getSlope(Pair start, Pair end) {
+    return log(start.y/end.y)/log(start.x/end.x);
+  }
+  
+  /**
+   * Returns the starting generating index used to generate the start Point.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   * @param xLogBase The logarithmic base for the x-coordinate
+   * @param ptsPerXBase The desired resolution specified in number of equally spaced points per 
+   * power of the x logarithmic base.  For example, if x is log-base2 specifying 4 would
+   * result in 4 equally spaced points for every power of 2 of x.
+   * @return the starting generating index used to generate the start Point
+   */
+  public static int getStartGenIndex(Pair start, Pair end, double xLogBase, int ptsPerXBase) {
+    double lnXbase = log(xLogBase);
+    boolean xIncreasing = end.x > start.x;
+    double iStartDbl = ptsPerXBase * log(start.x)/lnXbase;
+    return (int) ((xIncreasing)? Math.floor(iStartDbl) : Math.ceil(iStartDbl));
+  }
+  
+  /**
+   * Returns the ending generating index used to generate the end Point.
+   * @param start the desired starting (x,y) Pair
+   * @param end the desired ending (x,y) Pair
+   * @param xLogBase The logarithmic base for the x-coordinate
+   * @param ptsPerXBase The desired resolution specified in number of equally spaced points per 
+   * power of the x logarithmic base.  For example, if x is log-base2 specifying 4 would
+   * result in 4 equally spaced points for every power of 2 of x.
+   * @return the ending generating index used to generate the end Point
+   */
+  public static int getEndGenIndex(Pair start, Pair end, double xLogBase, int ptsPerXBase) {
+    double lnXbase = log(xLogBase);
+    boolean xIncreasing = end.x > start.x;
+    double iEndDbl = ptsPerXBase * log(end.x)/lnXbase;
+    return (int) ((!xIncreasing)? Math.floor(iEndDbl) : Math.ceil(iEndDbl));
+  }
+  
+  /**
+   * Returns the x-coordinate given the generating index.
+   * @param genIndex
+   * @param xLogBase The logarithmic base for the x-coordinate
+   * @param ptsPerXBase The desired resolution specified in number of equally spaced points per 
+   * power of the x logarithmic base.  For example, if x is log-base2 specifying 4 would
+   * result in 4 equally spaced points for every power of 2 of x.
+   * @return the x-coordinate based on the generating index
+   */
+  public static double getX(int genIndex, double xLogBase, int ptsPerXBase) {
+    return Math.pow(xLogBase, ((double)genIndex)/ptsPerXBase);
+  }
+  
+  /**
+   * Returns the y-coordinate given the computed x-coordinate and slope.
+   * @param start the desired starting (x,y) Pair
+   * @param slope
+   * @param x the computed x-coordinate
+   * @return the y-coordinate
+   */
+  public static double getY(Pair start, double slope, double x) {
+    return start.y * Math.exp(slope * log(x/start.x));
+  }
+  
+}
---
+++ src/test/java/com/yahoo/sketches/PowerLawGeneratorTest.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+package com.yahoo.sketches;
+
+//import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.yahoo.sketches.PowerLawGenerator.Pair;
+
+
+/**
+ * This provides sample code that generates tables of values that can be used for plotting or 
+ * other analytic computations. The first column is just the point number. The second column is
+ * the generating index for the coordinate pair.  The third column represents the "X-axis" and the
+ * last column represents the "Y-axis".  The first two columns are informational only.
+ */
+public class PowerLawGeneratorTest {
+  
+  //FIRST CODE EXAMPLE
+  
+  /**
+   * Generate a table of (x,y) Pairs.<br>
+   * X varies from 1 to 2^20.<br>
+   * Y varies from 2^20 to 1.<br>
+   * The x-axis is log-base2.<br>
+   * The resolution is 2 points per power of 2.<br>
+   * Skip duplicate integers for the X-coordinate.
+   */
+  @Test
+  public void testPLG() {
+    Pair start = new Pair(1, 1<<20);
+    Pair end = new Pair(1<<20, 1);
+    double xLogBase = 2.0;
+    int ptsPerXBase = 2;
+    
+    PowerLawGenerator plg = new PowerLawGenerator(xLogBase, ptsPerXBase, start, end);
+    int numGenIdxs = plg.getNumGenIndices();
+    int startGenIdx = plg.getStartGenIndex();
+    int del = plg.getDelta();
+    
+    println("Slope: "+plg.getSlope());
+    
+    Pair cur = plg.getPair(startGenIdx);
+    /*
+     * The iteration variable "i" guarantees that no more than numGenIdxs are output. 
+     * Fewer than numGenIdxs may be output based on the user-configured skip method.
+     * The iteration variable "gi" is the generating index and can subsequently increase or 
+     * decrease based on whether del == 1 or -1.
+     * The iteration variable "row" just counts the actual output rows for convenience.
+     */
+    for (int i=1, gi=startGenIdx, row=1; i<=numGenIdxs; i++, gi += del) {
+      if (gi != startGenIdx) {
+        Pair next = plg.getPair(gi);
+        if (skip(cur, next)) continue;
+        cur = next;
+      }
+      outputPair(row++, gi, cur);
+    }
+    
+  }
+  
+  /**
+   * Returns true if the next Pair should be skipped. Change this function to the desired criteria.
+   * @param cur the current Pair
+   * @param next the next Pair
+   * @return true to skip the next pair
+   */
+  private static boolean skip(Pair cur, Pair next) {
+    return ((int)next.x == (int)cur.x);
+  }
+  
+  /**
+   * Place-holder for a plotting or other analysis function
+   * @param row the row number (for convenience)
+   * @param gi generating index (for convenience)
+   * @param p the point Pair
+   */
+  private static void outputPair(int row, int gi, Pair p) {
+    int x = (int)p.x;
+    int y = (int)p.y;
+    println(row+"\t"+gi+"\t"+x+"\t"+y);
+  }
+  
+  // SECOND CODE EXAMPLE
+  
+  
+  /**
+   * Generate a series of equally spaced values for a log axis using static methods.<br>
+   * X varies from 1E9 to 10. (backwards)<br>
+   * The x-axis is log-base10.<br>
+   * The resolution is 5 points per power of 10.<br>
+   * Skip duplicate integers
+   */
+  @Test
+  public void testPLG2() {
+    Pair start = new Pair(1E9, 1); //Y is not used
+    Pair end   = new Pair(10, 1);  //Y is not used
+    double xLogBase = 10.0;
+    int ptsPerXBase = 5;
+    
+    int numGenIdxs = PowerLawGenerator.getNumGenIndices(start, end, xLogBase, ptsPerXBase);
+    int startGenIdx = PowerLawGenerator.getStartGenIndex(start, end, xLogBase, ptsPerXBase);
+    int del = PowerLawGenerator.getDelta(start, end);
+    
+    double cur = PowerLawGenerator.getX(startGenIdx, xLogBase, ptsPerXBase);
+    /*
+     * The iteration variable "i" guarantees that no more than numGenIdxs are output. 
+     * Fewer than numGenIdxs may be output based on the user-configured skip method.
+     * The iteration variable "gi" is the generating index and can subsequently increase or 
+     * decrease based on whether del == 1 or -1.
+     * The iteration variable "row" just counts the actual output rows for convenience.
+     */
+    for (int i=1, gi=startGenIdx, row=1; i<=numGenIdxs; i++, gi += del) {
+      if (gi != startGenIdx) {
+        double next = PowerLawGenerator.getX(gi, xLogBase, ptsPerXBase);
+        if (skip(cur, next)) continue;
+        cur = next;
+      }
+      outputX(row++, gi, cur);
+    }
+    
+  }
+  
+  /**
+   * Returns true if cur == next. Change this function to the desired criteria.
+   * @param cur the current value of x
+   * @param next the next value of x
+   * @return true if cur == next
+   */
+  private static boolean skip(double cur, double next) {
+    return ((int)next == (int)cur);
+  }
+  
+  /**
+   * Place-holder for a plotting or other analysis function
+   * @param row the row number (for convenience)
+   * @param gi generating index (for convenience)
+   * @param x the value x
+   */
+  private static void outputX(int row, int gi, double x) {
+    println(row+"\t"+gi+"\t"+((int)x));
+  }
+  
+  /**
+   * @param s value to print 
+   */
+  private static void println(String s) {
+    //System.out.println(s); //disable here
+  }
+  
+}
