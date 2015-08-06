31b2bb1e88de73f2021a6d1662b9291a7e689c5b
Lee Rhodes <lrhodes@yahoo-inc.com>
2015-08-06 12:16:19 -0700

    MemoryRegion state variables are now volatile.

--- src/main/java/com/yahoo/sketches/memory/package-info.java
+++ src/main/java/com/yahoo/sketches/memory/package-info.java
@@ -2,9 +2,9 @@
  * <p>The memory package contains classes that extend the Java Unsafe class and provide
  * direct primitive and array access to off-heap memory as well as compatible views into heap-based 
  * arrays and the Java ByteBuffer class.  These classes provide the foundation for building and 
- * managing sketchs and set operations in off-heap memory.  This package is general purpose in that 
- * it will find use for many other applications that need to manage data structures outside the 
- * Java heap.
+ * managing sketchs and set operations in off-heap memory.  This package is general purpose,
+ * has no external dependencies and can be used in any application that needs to manage data 
+ * structures outside the Java heap.
  * </p>
  * 
  * @author Lee Rhodes
