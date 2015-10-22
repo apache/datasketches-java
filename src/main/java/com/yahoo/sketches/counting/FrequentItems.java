6d3a75b26051abfb4f227e8926d668fd5ca6ceac
Edo Liberty <edo@yahoo-inc.com>
2015-10-21 20:51:49 -0400

    First version of frequent items

--- src/main/java/com/yahoo/sketches/counting/FrequentItems.java
+++ src/main/java/com/yahoo/sketches/counting/FrequentItems.java
@@ -1,48 +1,153 @@
 package com.yahoo.sketches.counting;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map.Entry;
+//import java.util.List;
 
 import com.yahoo.sketches.Util;
-import com.yahoo.sketches.hash.MurmurHash3;
+import static com.yahoo.sketches.hash.MurmurHash3.hash;
 
 
-//@SuppressWarnings("cast")
+@SuppressWarnings("cast")
 public class FrequentItems {
   
-  private HashMap<long[],Integer> counts;
+  private HashMap<Long,Integer> counts;
+  private int maxSize;
+  private ArrayList<Long> keysToDelete;
+  private int maxDeletesPerKey;
   
-  public FrequentItems(int size) {	  
-	  this.counts = new HashMap<long[],Integer>(size);
+  public FrequentItems(int maxSize) {
+	  this.maxSize = maxSize;
+	  this.counts = new HashMap<Long,Integer>(maxSize);
+	  this.keysToDelete = new ArrayList<Long>();
+	  this.maxDeletesPerKey = 0;
   }
 	
+  
+  
+  public void update(int key) {
+    updateWithHash(hash(new int[] {key}, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  
+  public Integer getEstimate(long[] key) {
+  	return getEstimateWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+
+  public Integer getUpperBound(int key) {
+  	return getUpperBoundWithHash(hash(new int[] {key}, Util.DEFAULT_UPDATE_SEED));
+  }
+
+  public FrequentItems union(FrequentItems that) {
+  	// Summing up the counts
+  	for (Entry<Long, Integer> entry : that.counts.entrySet()) {
+  		Long key = entry.getKey();
+  		int estimate = (Integer) this.counts.get(key);
+  		this.counts.put(key, estimate + entry.getValue());
+  	}
+  	// The count for a specific key could have been
+  	// deleted the maximal number of times in both sketches
+  	this.maxDeletesPerKey += that.maxDeletesPerKey;
+  	
+  	if (counts.size() >= maxSize){ 
+	  	// Crude way to find mind the value of the maxSize'th element in the array
+	  	ArrayList<Integer> valuesArray = new ArrayList<Integer>(that.counts.values());
+	  	Collections.sort(valuesArray);
+	  	int theresholdValue = valuesArray.get(maxSize-1);
+	  	
+	  	// Decrementing all counts by theresholdValue  
+	  	for (Entry<Long, Integer> entry : counts.entrySet()) {
+		    if (entry.getValue() <= theresholdValue){
+		    	keysToDelete.add(entry.getKey());
+		    } else {
+		    	counts.put(entry.getKey(), entry.getValue() - theresholdValue);
+		    }  
+			}
+	  	maxDeletesPerKey += theresholdValue;
+	  	
+	  	// Deleting counts that are below the threshold 
+			for (Long tempHash : keysToDelete) {
+				counts.remove(tempHash);
+			}
+			keysToDelete.clear();
+	  }
+		// returning a pointer to self
+  	return this;
+  }
+  
+  
   public void update(long[] key) {
-    updateWithHash(MurmurHash3.hash(key, Util.DEFAULT_UPDATE_SEED));
+    updateWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
   }
   
   public void update(int[] key) {
-    updateWithHash(MurmurHash3.hash(key, Util.DEFAULT_UPDATE_SEED));
+    updateWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  public Integer getEstimate(int key) {
+  	return getEstimateWithHash(hash(new int[] {key}, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  public Integer getEstimate(int[] key) {
+  	return getEstimateWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  public Integer getUpperBound(long[] key) {
+  	return getUpperBoundWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  public Integer getUpperBound(int[] key) {
+  	return getUpperBoundWithHash(hash(key, Util.DEFAULT_UPDATE_SEED));
+  }
+  
+  public int getSize() {
+	  return counts.size();
+  }
+  
+  public int getDeletePhases() {
+  	return maxDeletesPerKey;
   }
   
   private void updateWithHash(long[] hash) {
-	if(counts.containsKey(hash)){
-		counts.put(hash, counts.get(hash)+1);
-	}
-	//This is where the update function should do something
+  	Long half = hash[0];
+  	if(counts.containsKey(half)){
+			counts.put(half, counts.get(half)+1);
+		} else {
+			counts.put(half,1);
+		}
+			
+		// In case the hash map is full we delete a few entries. 
+  	// It is guaranteed that at least one entry will be deleted.
+		if (counts.size() >= maxSize) {
+			for (Entry<Long, Integer> entry : counts.entrySet()) {
+		    if (entry.getValue() <= 1){
+		    	keysToDelete.add(entry.getKey());
+		    } else {
+		    	counts.put(entry.getKey(), entry.getValue()-1);
+		    }  
+			}
+			
+			maxDeletesPerKey += 1; 
+			for (Long tempHash : keysToDelete) {
+				counts.remove(tempHash);
+			}
+			keysToDelete.clear();
+		}
   }
   
-  public double getEstimate(int[] key) { 
-	// return approx count + half max error 
-	  return 0.0;
+  private Integer getEstimateWithHash(long[] hash) {
+  	Long half = hash[0];
+  	return (counts.containsKey(half)) ? counts.get(half) : 0;
   }
 
-  public double getUpperBound(int[] key) {
-    // return approx count + max error
-    return 0.0;
+  private Integer getUpperBoundWithHash(long[] hash) {
+  	int lowerBound = getEstimateWithHash(hash) + maxDeletesPerKey;
+  	return (lowerBound > 0) ? lowerBound : 0;
   }
-
-  public double getLowerBound(int[] key) {
-	// return approx count
-    return 0.0;
-  }
-
+  
+  
 }
--- src/test/java/com/yahoo/sketches/counting/FrequentItemsTest.java
+++ src/test/java/com/yahoo/sketches/counting/FrequentItemsTest.java
@@ -1,15 +1,144 @@
 package com.yahoo.sketches.counting;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.lang.Math;
+import java.util.HashMap;
+
+/**
+ * @author edo
+ *
+ */
 
 public class FrequentItemsTest {
-	
-  @Test
+
+	@Test
   public void construct() {
     int size = 100;
-    FrequentItems f = new FrequentItems(size);
-    Assert.assertNotNull(f);
+    FrequentItems frequentItems = new FrequentItems(size);
+    AssertJUnit.assertNotNull(frequentItems);
   }
 		
+  @Test
+  public void updateOneTime() {
+    int size = 100;
+    FrequentItems frequentItems = new FrequentItems(size);
+    long[] key = new long[] {13};
+    frequentItems.update(key);
+    AssertJUnit.assertEquals(frequentItems.getSize(), 1);
+  }
+ 
+  @Test
+  public void sizeDoesNotGrow() {
+    int maxSize = 100;
+    FrequentItems frequentItems = new FrequentItems(maxSize);
+    for (int key=0; key<10000; key++){
+    	frequentItems.update(key);
+      AssertJUnit.assertTrue(frequentItems.getSize() <= maxSize);
+    }
+  }
+  
+  @Test
+  public void estimatesAreCorectBeofreDeletePhase() {
+    int maxSize = 100;
+    FrequentItems frequentItems = new FrequentItems(maxSize);
+    for (int key=0; key<99; key++){
+    	frequentItems.update(key);
+    	AssertJUnit.assertTrue(frequentItems.getEstimate(key) == 1);
+    	AssertJUnit.assertTrue(frequentItems.getDeletePhases() == 0);
+    }
+  }
+  
+  //Draws a number from a skewed distribution (geometric)
+  private int randomGeometricDist(double prob){
+  	return (int) (Math.log(Math.random()) / Math.log(1.0 - prob));
+  }
+  
+  @Test
+  public void testRandomGeometricDist() {
+  	int maxKey = 0;
+    double prob = .1;
+    for (int i=0; i<100; i++){
+    	// Draws a number from a skewed distribution (geometric)
+    	int key = randomGeometricDist(prob) ;
+    	if (key > maxKey) maxKey = key;
+    	//System.out.println(key);
+    	Assert.assertTrue((double)maxKey < 20.0/prob);
+    }
+  }
+   
+  @Test
+  public void realCountsInBounds() {
+    int n = 4213;
+    int maxSize = 50;
+    int key;
+    double prob = .04;
+    HashMap<Integer,Integer> realCounts = new HashMap<Integer,Integer>();
+   
+    FrequentItems frequentItems = new FrequentItems(maxSize);
+    for (int i=0; i<n; i++){ 	
+    	key = randomGeometricDist(prob);
+    	
+    	// Updating the real counters
+    	if (! realCounts.containsKey(key)) realCounts.put(key,0); 
+    	realCounts.put(key, realCounts.get(key)+1);
+    	
+    	frequentItems.update(key);
+    	
+    	AssertJUnit.assertTrue(realCounts.get(key) >= frequentItems.getEstimate(key));
+    	AssertJUnit.assertTrue(realCounts.get(key) <= frequentItems.getUpperBound(key)); 	
+    }
+  }
+  
+  @Test
+  public void errorWithinLimits() {
+    int n = 8713;
+    int maxSize = 50;
+    int key;
+    double p = .04;
+    FrequentItems frequentItems = new FrequentItems(maxSize);
+    for (int i=0; i<n; i++){
+    	// Draws a number from a skewed distribution (geometric)
+    	key = (int) -(Math.log(Math.random()) / p);
+    	frequentItems.update(key);
+    	AssertJUnit.assertTrue(frequentItems.getUpperBound(key) - frequentItems.getEstimate(key) <= i/maxSize);	
+    }
+  } 
+    
+  @Test
+  public void testUnionErrorWithinBounds() {
+    int n = 1000;
+    int maxSize1 = 100;
+    int maxSize2 = 400;
+    double prob1 = .01;
+    double prob2 = .005;
+   
+    HashMap<Integer,Integer> realCounts = new HashMap<Integer,Integer>();
+    FrequentItems frequentItems1 = new FrequentItems(maxSize1);
+    FrequentItems frequentItems2 = new FrequentItems(maxSize2);
+    for (int i=0; i<n; i++){
+    	int key1 =randomGeometricDist(prob1);
+    	frequentItems1.update(key1);
+    	int key2 =randomGeometricDist(prob2);
+    	frequentItems1.update(key2);
+    	
+    	// Updating the real counters
+    	if (! realCounts.containsKey(key1)) realCounts.put(key1,0); 
+    	realCounts.put(key1, realCounts.get(key1)+1);
+    	// Updating the real counters
+    	if (! realCounts.containsKey(key2)) realCounts.put(key2,0); 
+    	realCounts.put(key2, realCounts.get(key2)+1);
+    }
+    FrequentItems frequentItems = frequentItems1.union(frequentItems2);
+
+    for ( Integer key : realCounts.keySet()){
+    	AssertJUnit.assertTrue(realCounts.get(key) >= frequentItems.getEstimate(key));
+    	AssertJUnit.assertTrue(realCounts.get(key) <= frequentItems.getUpperBound(key));
+    }
+  	 	
+  }
+  
+  
 }
