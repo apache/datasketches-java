03590b5a1879f1cdcca55bdbd7c1e881aaf38fe0
Lee Rhodes <lrhodes@yahoo-inc.com>
2015-11-03 15:25:36 -0800

    fix typos

--- src/main/java/com/yahoo/sketches/memory/package-info.java
+++ src/main/java/com/yahoo/sketches/memory/package-info.java
@@ -2,7 +2,7 @@
  * <p>The memory package contains classes that extend the Java Unsafe class and provide
  * direct primitive and array access to off-heap memory as well as compatible views into heap-based 
  * arrays and the Java ByteBuffer class.  These classes provide the foundation for building and 
- * managing sketchs and set operations in off-heap memory.  This package is general purpose,
+ * managing sketches and set operations in off-heap memory.  This package is general purpose,
  * has no external dependencies and can be used in any application that needs to manage data 
  * structures outside the Java heap.
  * </p>
