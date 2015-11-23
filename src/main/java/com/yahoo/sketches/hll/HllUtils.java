baadd0840c6f4db6c1cfa8f6dfdc0cdf9e6ef454
Lee Rhodes <lrhodes@yahoo-inc.com>
2015-11-23 12:12:47 -0800

    Eliminate invPower2Table with inline calculation suggested by Otmar Ertl

--- src/main/java/com/yahoo/sketches/hll/HllUtils.java
+++ src/main/java/com/yahoo/sketches/hll/HllUtils.java
@@ -1,21 +1,24 @@
+/*
+ * Copyright 2015, Yahoo! Inc.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
 package com.yahoo.sketches.hll;
 
 /**
+ * Utility functions for the HLL package
+ * 
+ * @author Eric Tschetter
+ * @author Kevin Lang
  */
-class HllUtils
-{
-  static double[] invPow2Table = new double[256];
-  static {
-    for (int i = 0; i < 256; i++) invPow2Table[i] = Math.pow(2.0, -1.0 * i);
-  }
+class HllUtils {
 
   static double computeInvPow2Sum(int numBuckets, BucketIterator iter) {
     double retVal = 0;
     while (iter.next()) {
-      retVal += invPow2Table[iter.getValue()];
+      retVal += invPow2(iter.getValue());
       --numBuckets;
     }
-    retVal += numBuckets * invPow2Table[0];
+    retVal += numBuckets;
     return retVal;
   }
 
@@ -26,4 +29,14 @@ class HllUtils
     return fields;
   }
 
+  /**
+   * Computes the inverse integer power of 2: 1/(2^e) == 2^(-e). 
+   * @param e a positive value between 0 and 1023 inclusive
+   * @return  the inverse integer power of 2: 1/(2^e) == 2^(-e)
+   */
+  static final double invPow2(int e) {
+    assert (e | (1024 - e -1)) >= 0 : "e cannot be negative or greater than 1023: "+ e;
+    return Double.longBitsToDouble((0x3ffL - e) << 52); //suggested by Otmar Ertl
+  }
+  
 }
