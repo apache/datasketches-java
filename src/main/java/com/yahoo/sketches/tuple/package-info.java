d4bc3f2d449180df9779f347d03c1589b6aabc10
saydakov <saydakov@yahoo-inc.com>
2016-02-19 12:17:42 -0800

    package info

--- src/main/java/com/yahoo/sketches/tuple/package-info.java
+++ src/main/java/com/yahoo/sketches/tuple/package-info.java
@@ -1,7 +1,11 @@
 /**
+ * The tuple package contains implementation of sketches based on the idea of
+ * theta sketches with the addition of values associated with unique keys.
+ * Two sets of tuple sketch classes are available at the moment:
+ * generic tuple sketches with user-defined Summary, and a faster specialized
+ * implementation with an array of double values.
+ * See unit tests for usage examples.
  *
- * @see <a href="datasketches.github.io">DataSketches.GitHub.io</a>
- * 
  * @author Alexander Saydakov
  */
 package com.yahoo.sketches.tuple;
