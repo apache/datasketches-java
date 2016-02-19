12f00eb2a3ae813f786e8538093b89917debc38a
saydakov <saydakov@yahoo-inc.com>
2016-02-19 15:11:59 -0800

    implemented a-not-b

---
+++ src/main/java/com/yahoo/sketches/tuple/ArrayOfDoublesAnotB.java
@@ -0,0 +1,34 @@
+package com.yahoo.sketches.tuple;
+
+import com.yahoo.sketches.memory.Memory;
+
+/**
+ * This is to compute a set difference of two tuple sketches
+ */
+public abstract class ArrayOfDoublesAnotB {
+
+  /**
+   * Perform A-and-not-B set operation on the two given sketches.
+   * A null sketch is interpreted as an empty sketch.
+   * This is not an accumulating update. Calling update() more than once
+   * without calling getResult() will discard the result of previous update()
+   * 
+   * @param a The incoming sketch for the first argument
+   * @param b The incoming sketch for the second argument
+   */  
+  public abstract void update(ArrayOfDoublesSketch a, ArrayOfDoublesSketch b);
+
+  /**
+   * Gets the result of this operation in the form of a ArrayOfDoublesCompactSketch
+   * @return compact sketch representing the result of the operation
+   */
+  public abstract ArrayOfDoublesCompactSketch getResult();
+
+  /**
+   * Gets the result of this operation in the form of a ArrayOfDoublesCompactSketch
+   * @param mem memory for the result (can be null)
+   * @return compact sketch representing the result of the operation (off-heap if memory is provided)
+   */
+  public abstract ArrayOfDoublesCompactSketch getResult(Memory mem);
+
+}
