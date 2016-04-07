75c11d928a5835bde0744b467d1f0b2577ae5bf7
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-04-06 18:48:42 -0700

    Disable println in HashMapTest

--- src/test/java/com/yahoo/sketches/frequencies/HashMapTest.java
+++ src/test/java/com/yahoo/sketches/frequencies/HashMapTest.java
@@ -131,7 +131,7 @@ public class HashMapTest {
    * @param s value to print
    */
   static void println(String s) {
-    System.out.println(s); //disable here
+    //System.out.println(s); //disable here
   }
   
 }
