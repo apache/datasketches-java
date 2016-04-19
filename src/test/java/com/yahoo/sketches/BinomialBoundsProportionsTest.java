42f6988fd5cdfd8a656175deb602fe5928dc0137
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-04-19 14:38:22 -0700

    Disable test printing

--- src/test/java/com/yahoo/sketches/BinomialBoundsProportionsTest.java
+++ src/test/java/com/yahoo/sketches/BinomialBoundsProportionsTest.java
@@ -106,7 +106,7 @@ public class BinomialBoundsProportionsTest {
    * @param s value to print 
    */
   static void println(String s) {
-    System.out.println(s); //disable here
+    //System.out.println(s); //disable here
   }
   
   
