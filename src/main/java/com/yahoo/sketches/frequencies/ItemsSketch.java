58824729a3f7e90680e7ec54c872fa4e5008b465
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-07-07 12:58:21 -0700

    Corrections to hashCode() and equals for Row ordering to insure compatability with compareTo.

--- src/main/java/com/yahoo/sketches/frequencies/ItemsSketch.java
+++ src/main/java/com/yahoo/sketches/frequencies/ItemsSketch.java
@@ -519,22 +519,41 @@ public class ItemsSketch<T> {
       return String.format(FMT,  est, ub, lb, item.toString());
     }
 
+    /**
+     * This compareTo is strictly limited to the Row.getEstimate() value and does not imply any 
+     * ordering whatsoever to the other elements of the row: item and upper and lower bounds. 
+     * Defined this way, this compareTo will be consistent with hashCode() and equals(Object).
+     * @param that the other row to compare to.
+     * @return a negative integer, zero, or a positive integer as this.getEstimate() is less than, 
+     * equal to, or greater than that.getEstimate().
+     */
     @Override
     public int compareTo(final Row<T> that) {
       return (this.est < that.est) ? -1 : (this.est > that.est) ? 1 : 0;
     }
     
+    /**
+     * This hashCode is computed only from the Row.getEstimate() value. 
+     * Defined this way, this hashCode will be consistent with equals(Object):<br>
+     * If (x.equals(y)) implies: x.hashCode() == y.hashCode().<br>
+     * If (!x.equals(y)) does NOT imply: x.hashCode() != y.hashCode().
+     * @return the hashCode computed from getEstimate().
+     */
     @Override
     public int hashCode() {
       final int prime = 31;
       int result = 1;
       result = prime * result + (int) (est ^ (est >>> 32));
-      result = prime * result + ((item == null) ? 0 : item.hashCode());
-      result = prime * result + (int) (lb ^ (lb >>> 32));
-      result = prime * result + (int) (ub ^ (ub >>> 32));
       return result;
     }
 
+    /**
+     * This equals is computed only from the Row.getEstimate() value and does not imply equality 
+     * of the other items within the row: item and upper and lower bounds.
+     * Defined this way, this equals will be consistent with compareTo(Row).
+     * @param obj the other row to determine equality with.
+     * @return true if this.getEstimate() equals ((Row&lt;T&gt;)obj).getEstimate().
+     */
     @SuppressWarnings("unchecked")
     @Override
     public boolean equals(Object obj) {
@@ -546,7 +565,7 @@ public class ItemsSketch<T> {
       return true;
     }
     
-  }
+  } //End of class Row<T>
 
   Row<T>[] sortItems(final long threshold, final ErrorType errorType) {
     final ArrayList<Row<T>> rowList = new ArrayList<Row<T>>();
