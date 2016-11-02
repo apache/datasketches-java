d5e823a2decec8f57255bb39e66b360a958a81b4
saydakov <saydakov@yahoo-inc.com>
2016-11-02 16:04:30 -0700

    better doc

--- sketches/src/main/java/com/yahoo/sketches/hllmap/package-info.java
+++ sketches/src/main/java/com/yahoo/sketches/hllmap/package-info.java
@@ -3,16 +3,23 @@
  * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
  */
 
+package com.yahoo.sketches.hllmap;
+
 /**
  * Space efficient map of n-byte keys to an approximate unique count of values.
- * Example: number of users per IPv4 address (4 bytes)
- * UniqueCountMap map = new UniqueCountMap(4);
- * for (String userId: listOfUserIds) {
- *   double uniqCountEstimateSoFar = map.update("1234".getBytes(), userId.getBytes());
- * }
+ * Example: number of users per IPv4 address (4 bytes),
+ * expected number of unique IP addresses is 100,000,
+ * and HLL parameter k=1024
+ *
+ * UniqueCountMap map = new UniqueCountMap(100_000, 4, 1024);
+ * InetAddress inetAddress;
+ * String userId;
+ * ...
+ * double uniqCountEstimateSoFar = map.update(inetAddress.getAddress(), userId.getBytes());
+ * 
+ * @see UniqueCountMap
  * 
  * @author Kevin Lang 
  * @author Lee Rhodes
  * @author Alexander Saydakov
  */
-package com.yahoo.sketches.hllmap;
