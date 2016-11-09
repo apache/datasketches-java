68b0c056661c50d1d66badfff05d0b2f8c6c53f5
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-11-09 11:13:08 -0800

    Code and javadoc cleanup

--- sketches/src/main/java/com/yahoo/sketches/hll/UniqueCountMap.java
+++ sketches/src/main/java/com/yahoo/sketches/hll/UniqueCountMap.java
@@ -8,23 +8,25 @@ package com.yahoo.sketches.hll;
 import com.yahoo.sketches.SketchesArgumentException;
 
 /**
- * This is a real-time, key-value mapping sketch that tracks approximate unique counts of
+ * This is a real-time, key-value HLL mapping sketch that tracks approximate unique counts of
  * identifiers (the values) associated with each key. An example might be tracking the number of
  * unique user identifiers associated with each IP address. This map has been specifically designed
  * for the use-case where the number of keys is quite large (many millions) and the distribution of
  * identifiers per key is very skewed. A typical distribution where this works well is a
- * power-law distribution of identifiers per key of the form <i>p(x) = Cx<sup>-&alpha;</sup></i>,
- * where <i>&alpha;</i> &lt; 0.5. Assuming 100M keys, over 75% of the keys would have only one
- * identifier, 99% of the keys would have less than 20 identifiers, etc.
+ * power-law distribution of identifiers per key of the form <i>y = Cx<sup>-&alpha;</sup></i>,
+ * where <i>&alpha;</i> &lt; 0.5, and <i>C</i> is roughly <i>y<sub>max</sub></i>.
+ * For example, with 100M keys, over 75% of the keys would have only
+ * one identifier, 99% of the keys would have less than 20 identifiers, 99.9% would have less than
+ * 200 identifiers, and a very tiny fraction might have identifiers in the thousands.
  *
  * <p>The space consumed by this map is quite sensitive to the actual distribution of identifiers
- * per key, so you should characterize and or experiment with your typical input streams beforehand.
+ * per key, so you should characterize and or experiment with your typical input streams.
  * Nonetheless, our experiments on live streams of over 100M keys required space less than 2GB.
  *
  * <p>Given such highly-skewed distributions, using this map is far more efficient space-wise than
- * the alternative of dedicating an HLL sketch per key. Based on our measurements, after
+ * the alternative of dedicating an HLL sketch per key. Based on our use cases, after
  * subtracting the space required for key storage, the average bytes per key required for unique
- * count estimation is about 10.
+ * count estimation ({@link  #getAverageSketchMemoryPerKey()}) is about 10.
  *
  * <p>Internally, this map is implemented as a hierarchy of hash maps with progressively
  * increasing storage allocated for unique count estimation. As a key acquires more identifiers it
@@ -34,28 +36,29 @@ import com.yahoo.sketches.SketchesArgumentException;
  * <p>The unique values in all the levels, except the final HLL map, are stored in a special form
  * called a coupon. A coupon is a 16-bit value that fully describes a k=1024 HLL bin. It contains
  * 10 bits of address and a 6-bit number, which represents the number of leading zeroes in a
- * 64-bit hash plus one to make it non-zero.
+ * 64-bit hash of the identifier, plus one to make it non-zero.
  *
- * <p>All hash tables here have prime size to reduce wasted space compared to powers of two.
- * Open addressing with the second hash is used to resolve collisions.
+ * <p>All hash tables use a prime number size and use Knuth's Open Addressing Double Hash (OADH)
+ * search algorithm.
  *
  * <p>The base table holds all the keys and each key is associated with one 16-bit value. Initially,
  * the value is a single coupon. Once the key is promoted, this 16-bit field contains a reference
- * to the map level where the key is still active.
+ * to the map table where the key is still active.
  *
- * <p>The intermediate maps between the base level map and the final HLL map are of two types.
+ * <p>The intermediate map tables between the base level map and the final HLL map are of two types.
  * The first few of these are called traverse maps where the coupons are
  * stored as unsorted arrays. After the traverse maps are the coupon hash maps, where the coupons
- * are stored in small hash tables.
+ * are stored in small OASH hash tables.
  *
- * <p>All the intermediate maps support deletes which allows reuse and enables the
- * intermediate tables to dynamically grow and shrink.
+ * <p>All the intermediate maps support deletes and can dynamically grow and shrink as required by
+ * the input stream.
  *
- * <p>This approach provides unbiased unique count estimates with Relative Standard Error (RSE)
+ * <p>The sketch estimator algorithms are unbiased with a Relative Standard Error (RSE)
  * of about 2.5% with 68% confidence, or equivalently, about 5% with a 95% confidence.
  *
  * <p>There are 2 classes in the test hierarchy that can be used from the command line to feed this
- * mapping sketch piped from standard-in. The first is ProcessIpStream, which processes IP/ID pairs
+ * mapping sketch piped from standard-in for experimental evaluation.
+ * The first is ProcessIpStream, which processes simple IP/ID pairs
  * and the second, ProcessDistributionStream, which processes pairs that describe a distribution.
  * Please refer to the javadocs for those classes for more information.
  *
@@ -69,6 +72,7 @@ public class UniqueCountMap {
   private static final int NUM_TRAVERSE_LEVELS = 3;
   private static final int HLL_K = 1024;
   private static final int INITIAL_NUM_ENTRIES = 1000003;
+  private static final int MIN_INITIAL_NUM_ENTRIES = 157;
   private final int keySizeBytes_;
 
   private final SingleCouponMap baseLevelMap;
@@ -87,10 +91,11 @@ public class UniqueCountMap {
   }
 
   /**
-   * Constructs a UniqueCountMap. The initial number of entries provides a tradeoff between
+   * Constructs a UniqueCountMap with a given initial number of entries.
+   *
+   * @param initialNumEntries The initial number of entries provides a tradeoff between
    * wasted space, if too high, and wasted time resizing the table, if too low.
-   * @param initialNumEntries initial size of the base table
-   * @param keySizeBytes must be at least 4 bytes to have enough entropy
+   * @param keySizeBytes must be at least 4 bytes to have sufficient entropy
    */
   public UniqueCountMap(final int initialNumEntries, final int keySizeBytes) {
     checkTgtEntries(initialNumEntries);
@@ -102,15 +107,16 @@ public class UniqueCountMap {
   }
 
   /**
-   * Updates the map with a given key and value.
-   * @param key given key
-   * @param identifier given identifier for unique counting associated with the key
-   * @return estimate of unique count so far including the current identifier
+   * Updates the map with a given key and identifier and returns the estimate of the number of
+   * unique identifiers encountered so far for the given key.
+   * @param key the given key
+   * @param identifier the given identifier for unique counting associated with the key
+   * @return the estimate of the number of unique identifiers encountered so far for the given key.
    */
   public double update(final byte[] key, final byte[] identifier) {
     if (key == null) return Double.NaN;
     if (key.length != keySizeBytes_) {
-      throw new SketchesArgumentException("Key must be " + keySizeBytes_ + " bytes long");
+      throw new SketchesArgumentException("Key size must be " + keySizeBytes_ + " bytes.");
     }
     if (identifier == null) return getEstimate(key);
     final short coupon = (short) Map.coupon16(identifier);
@@ -126,7 +132,7 @@ public class UniqueCountMap {
       if (baseLevelMapCoupon == coupon) return 1; //duplicate
       // promote from the base level
       baseLevelMap.setCoupon(baseLevelIndex, (short) 1, true); //set coupon = Level 1; state = 1
-      CouponMap newMap = getIntermediateMapForLevel(1);
+      CouponMap newMap = getIntermediateMap(1);
       final int index = newMap.findOrInsertKey(key);
       newMap.findOrInsertCoupon(index, baseLevelMapCoupon);
       final double estimate = newMap.findOrInsertCoupon(index, coupon);
@@ -144,7 +150,7 @@ public class UniqueCountMap {
       level++;
       baseLevelMap.setCoupon(baseLevelIndex, (short) level, true); //set coupon = level number; state = 1
       if (level <= NUM_INTERMEDIATE_LEVELS) {
-        final CouponMap newMap = getIntermediateMapForLevel(level);
+        final CouponMap newMap = getIntermediateMap(level);
         final int newMapIndex = newMap.findOrInsertKey(key);
         final CouponsIterator it = map.getCouponsIterator(key);
         while (it.next()) {
@@ -170,18 +176,6 @@ public class UniqueCountMap {
       return estimate;
     }
     return lastLevelMap.update(key, coupon);
-  }
-
-  private CouponMap getIntermediateMapForLevel(final int level) {
-    if (intermediateLevelMaps[level - 1] == null) {
-      final int newLevelCapacity = 1 << level;
-      if (level <= NUM_TRAVERSE_LEVELS) {
-        intermediateLevelMaps[level - 1] = CouponTraverseMap.getInstance(keySizeBytes_, newLevelCapacity);
-      } else {
-        intermediateLevelMaps[level - 1] = CouponHashMap.getInstance(keySizeBytes_, newLevelCapacity);
-      }
-    }
-    return intermediateLevelMaps[level - 1];
   }
 
   /**
@@ -246,6 +240,14 @@ public class UniqueCountMap {
   }
 
   /**
+   * Returns the average memory storage per key that is dedicated to sketching the unique counts.
+   * @return the average memory storage per key that is dedicated to sketching the unique counts.
+   */
+  public double getAverageSketchMemoryPerKey() {
+    return (double) (getMemoryUsageBytes() - getKeyMemoryUsageBytes()) / getActiveEntries();
+  }
+
+  /**
    * Returns the number of active levels so far.
    * Only the base level table is initialized in the constructor, so this method would return 1.
    * As more keys are promoted up to the next levels, the return value would grow until the
@@ -271,7 +273,7 @@ public class UniqueCountMap {
     final long totKeys = getActiveEntries();
     final long totMem = getMemoryUsageBytes();
     final long keyMem = getKeyMemoryUsageBytes();
-    final double avgValMemPerKey = (double)(totMem - keyMem) / totKeys;
+    final double avgValMemPerKey = getAverageSketchMemoryPerKey();
 
     final String ksb = Map.fmtLong(keySizeBytes_);
     final String alvls  = Map.fmtLong(getActiveLevels());
@@ -284,12 +286,12 @@ public class UniqueCountMap {
     final StringBuilder sb = new StringBuilder();
     final String thisSimpleName = this.getClass().getSimpleName();
     sb.append("## ").append(thisSimpleName).append(" SUMMARY: ").append(LS);
-    sb.append("   Key Size Bytes            : ").append(ksb).append(LS);
-    sb.append("   Active Map Levels         : ").append(alvls).append(LS);
-    sb.append("   Total keys                : ").append(tKeys).append(LS);
-    sb.append("   Total Memory Bytes        : ").append(tMem).append(LS);
-    sb.append("   Total Key Memory Bytes    : ").append(kMem).append(LS);
-    sb.append("   Avg Value Memory Bytes/Key: ").append(avgValMem).append(LS);
+    sb.append("   Key Size Bytes             : ").append(ksb).append(LS);
+    sb.append("   Active Map Levels          : ").append(alvls).append(LS);
+    sb.append("   Total keys                 : ").append(tKeys).append(LS);
+    sb.append("   Total Memory Bytes         : ").append(tMem).append(LS);
+    sb.append("   Total Key Memory Bytes     : ").append(kMem).append(LS);
+    sb.append("   Avg Sketch Memory Bytes/Key: ").append(avgValMem).append(LS);
     sb.append(LS);
     sb.append(baseLevelMap.toString());
     sb.append(LS);
@@ -309,9 +311,21 @@ public class UniqueCountMap {
     return sb.toString();
   }
 
-  private static final void checkTgtEntries(final int tgtEntries) {
-    if (tgtEntries < 16) {
-      throw new SketchesArgumentException("tgtEntries must be >= 16");
+  private CouponMap getIntermediateMap(final int level) {
+    if (intermediateLevelMaps[level - 1] == null) {
+      final int newLevelCapacity = 1 << level;
+      if (level <= NUM_TRAVERSE_LEVELS) {
+        intermediateLevelMaps[level - 1] = CouponTraverseMap.getInstance(keySizeBytes_, newLevelCapacity);
+      } else {
+        intermediateLevelMaps[level - 1] = CouponHashMap.getInstance(keySizeBytes_, newLevelCapacity);
+      }
+    }
+    return intermediateLevelMaps[level - 1];
+  }
+
+  private static final void checkTgtEntries(final int initNumEntries) {
+    if (initNumEntries < MIN_INITIAL_NUM_ENTRIES) {
+      throw new SketchesArgumentException("initialNumEntries must be >= " + MIN_INITIAL_NUM_ENTRIES);
     }
   }
 
--- sketches/src/test/java/com/yahoo/sketches/hll/UniqueCountMapTest.java
+++ sketches/src/test/java/com/yahoo/sketches/hll/UniqueCountMapTest.java
@@ -13,10 +13,10 @@ import org.testng.annotations.Test;
 import com.yahoo.sketches.SketchesArgumentException;
 
 public class UniqueCountMapTest {
-
+  private final static int INIT_ENTRIES = 211;
   @Test
   public void nullKey() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     double estimate = map.update(null, null);
     Assert.assertEquals(estimate, Double.NaN);
     Assert.assertEquals(map.getEstimate(null), Double.NaN);
@@ -24,41 +24,41 @@ public class UniqueCountMapTest {
 
   @Test(expectedExceptions = SketchesArgumentException.class)
   public void wrongSizeKeyUpdate() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     byte[] key = new byte[] {0};
     map.update(key, null);
   }
 
   @Test(expectedExceptions = SketchesArgumentException.class)
   public void wrongSizeKey() {
-    UniqueCountMap map = new UniqueCountMap(16, 2);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 2);
     println(map.toString());
   }
 
   @Test(expectedExceptions = SketchesArgumentException.class)
   public void wrongSizeKeyGetEstimate() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     byte[] key = new byte[] {0};
     map.getEstimate(key);
   }
 
   @Test
   public void emptyMapNullValue() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     double estimate = map.update("1234".getBytes(UTF_8), null);
     Assert.assertEquals(estimate, 0.0);
   }
 
   @Test
   public void oneEntry() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     double estimate = map.update("1234".getBytes(UTF_8), "a".getBytes(UTF_8));
     Assert.assertEquals(estimate, 1.0, 0.01);
   }
 
   @Test
   public void duplicateEntry() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     byte[] key = "1234".getBytes(UTF_8);
     double estimate = map.update(key, "a".getBytes(UTF_8));
     Assert.assertEquals(estimate, 1.0);
@@ -70,7 +70,7 @@ public class UniqueCountMapTest {
 
   @Test
   public void oneKeyTwoValues() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     double estimate = map.update("1234".getBytes(UTF_8), "a".getBytes(UTF_8));
     Assert.assertEquals(estimate, 1.0);
     estimate = map.update("1234".getBytes(UTF_8), "b".getBytes(UTF_8));
@@ -79,7 +79,7 @@ public class UniqueCountMapTest {
 
   @Test
   public void oneKeyThreeValues() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     byte[] key = "1234".getBytes(UTF_8);
     double estimate = map.update(key, "a".getBytes(UTF_8));
     Assert.assertEquals(estimate, 1.0);
@@ -92,7 +92,7 @@ public class UniqueCountMapTest {
   @SuppressWarnings("unused")
   @Test
   public void oneKeyManyValues() {
-    UniqueCountMap map = new UniqueCountMap(16, 4);
+    UniqueCountMap map = new UniqueCountMap(INIT_ENTRIES, 4);
     byte[] key = "1234".getBytes(UTF_8);
     byte[] id = new byte[4];
     for (int i = 1; i <= 1000; i++) {
