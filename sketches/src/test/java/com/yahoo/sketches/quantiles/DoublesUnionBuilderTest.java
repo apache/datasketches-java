a9c876a7bf0a453d0365a2fe11e89be5dcfe4cdb
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-12-01 17:13:51 -0800

    Add SuppressWarnings for deprecated method

--- sketches/src/test/java/com/yahoo/sketches/quantiles/DoublesUnionBuilderTest.java
+++ sketches/src/test/java/com/yahoo/sketches/quantiles/DoublesUnionBuilderTest.java
@@ -14,40 +14,41 @@ import com.yahoo.memory.NativeMemory;
 
 public class DoublesUnionBuilderTest {
 
+  @SuppressWarnings("deprecation")
   @Test
   public void checkBuilds() {
     DoublesSketch qs1 = DoublesSketch.builder().build();
     for (int i=0; i<1000; i++) qs1.update(i);
-    
+
     int bytes = qs1.getStorageBytes();
     Memory dstMem = new NativeMemory(new byte[bytes]);
     qs1.putMemory(dstMem);
     Memory srcMem = dstMem;
-    
+
     DoublesUnionBuilder bldr = new DoublesUnionBuilder();
     bldr.setK(128);
     DoublesUnion union = bldr.build(); //virgin union
-    
+
     union = DoublesUnionBuilder.build(srcMem);
     DoublesSketch qs2 = union.getResult();
     assertEquals(qs1.getStorageBytes(), qs2.getStorageBytes());
-    
+
     union = DoublesUnionBuilder.copyBuild(qs2);
     DoublesSketch qs3 = union.getResult();
     assertEquals(qs2.getStorageBytes(), qs3.getStorageBytes());
     assertFalse(qs2 == qs3);
   }
-  
+
   @Test
   public void printlnTest() {
     println("PRINTING: "+this.getClass().getName());
   }
 
   /**
-   * @param s value to print 
+   * @param s value to print
    */
   static void println(String s) {
     //System.err.println(s); //disable here
   }
-  
+
 }
