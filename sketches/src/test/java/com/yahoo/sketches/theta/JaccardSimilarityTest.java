50cab4757f3df3ac4da0260560ac0c17fc9fe735
Lee Rhodes <lrhodes@yahoo-inc.com>
2017-01-05 12:17:03 -0800

    Disable printing

--- sketches/src/test/java/com/yahoo/sketches/theta/JaccardSimilarityTest.java
+++ sketches/src/test/java/com/yahoo/sketches/theta/JaccardSimilarityTest.java
@@ -183,7 +183,7 @@ public class JaccardSimilarityTest {
    * @param s value to print
    */
   static void println(String s) {
-    System.out.println(s); //disable here
+    //System.out.println(s); //disable here
   }
 
 }
