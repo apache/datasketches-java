3e1dd3cb984073e2073a98084fe6479344e32e72
jmalkin <jmalkin@yahoo-inc.com>
2017-03-22 11:02:09 -0700

    Fix javadoc to specify where returning UpdateDoublesSketch

--- sketches/src/main/java/com/yahoo/sketches/quantiles/DoublesSketchBuilder.java
+++ sketches/src/main/java/com/yahoo/sketches/quantiles/DoublesSketchBuilder.java
@@ -69,8 +69,8 @@ public class DoublesSketchBuilder {
   }
 
   /**
-   * Returns a DoublesSketch with the current configuration of this Builder.
-   * @return a DoublesSketch
+   * Returns an UpdateDoublesSketch with the current configuration of this Builder.
+   * @return a UpdateDoublesSketch
    */
   public UpdateDoublesSketch build() {
     return (bMem == null) ? HeapUpdateDoublesSketch.newInstance(bK)
@@ -78,7 +78,7 @@ public class DoublesSketchBuilder {
   }
 
   /**
-   * Returns a quantiles DoublesSketch with the current configuration of this builder and the
+   * Returns a quantiles UpdateDoublesSketch with the current configuration of this builder and the
    * given parameter <i>k</i>.
    * @param k determines the accuracy and size of the sketch.
    * <i>k</i> must be greater than 1 and less than 65536.
@@ -86,7 +86,7 @@ public class DoublesSketchBuilder {
    * different values of <i>k</i>. However, in this case it is only possible to merge from
    * larger values of <i>k</i> to smaller values.
    *
-   * @return a DoublesSketch
+   * @return an UpdateDoublesSketch
    */
   public UpdateDoublesSketch build(final int k) {
     return (bMem == null) ? HeapUpdateDoublesSketch.newInstance(k)
