9b7ad9cb2d226c6e1e476fb54ac0a3be7da11fd0
saydakov <saydakov@yahoo-inc.com>
2017-06-05 12:32:58 -0700

    javadoc fixes

--- src/main/java/com/yahoo/sketches/hllmap/package-info.java
+++ src/main/java/com/yahoo/sketches/hllmap/package-info.java
@@ -16,25 +16,13 @@
  * <p>From our own testing, sketching 100 million IPv4 addresses with such a
  * highly skewed distribution of identifiers per IP uses only 1.4GB of memory. This translates to
  * an average of about 10 bytes per IP allocated to the equivalent of a full k=1024 HLL sketch
- * and provides an RSE of less than 2.5%.  Your results will vary depending on the actual
+ * and provides an RSE of less than 2.5%. Your results will vary depending on the actual
  * distribution of identifiers per key.
- *
- *  * <p>The HLL sketch is not recommended if you anticipate the need of performing set intersection
- * or difference operations with reasonable accuracy,
- * or if you anticipate the need to merge sketches that were constructed with different
- * values of <i>k</i> or <i>Nominal Entries</i>.</p>
- *
- * <p>HLL sketches do not retain any of the hash values of the associated unique identifiers,
- * so if there is any anticipation of a future need to leverage associations with these
- * retained hash values, Theta Sketches would be a better choice.</p>
- *
- * <p>HLL sketches cannot be intermixed or merged in any way with Theta Sketches.
- * </p>
  *
  * @see com.yahoo.sketches.hllmap.UniqueCountMap
  *
  * @author Lee Rhodes
  * @author Kevin Lang
- * @author Alex Saydakov
+ * @author Alexander Saydakov
  */
 package com.yahoo.sketches.hllmap;
