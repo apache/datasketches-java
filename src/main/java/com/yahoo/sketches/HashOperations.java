f897dbc90e9dedc98b239a5651920c98bb0d4672
Jon Malkin <jon.malkin@gmail.com>
2017-10-13 09:44:59 -0700

    convert while loops to do..while, to allow checks for infinite loops

--- src/main/java/com/yahoo/sketches/HashOperations.java
+++ src/main/java/com/yahoo/sketches/HashOperations.java
@@ -19,6 +19,8 @@ import com.yahoo.memory.WritableMemory;
 public final class HashOperations {
   private static final int STRIDE_HASH_BITS = 7;
 
+  private static final int EMPTY = 0;
+
   /**
    * The stride mask for the Open Address, Double Hashing (OADH) hash table algorithm.
    */
@@ -90,11 +92,18 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    // search for duplicate or zero
-    while (hashTable[curProbe] != 0) {
-      if (hashTable[curProbe] == hash) { return curProbe; } // a duplicate
+
+    // search for duplicate or empty slot
+    final int loopIndex = curProbe;
+    do {
+      final long arrVal = hashTable[curProbe];
+      if (arrVal == EMPTY) {
+        return -1; // not found
+      } else if (arrVal == hash) {
+        return curProbe; // found
+      }
       curProbe = (curProbe + stride) & arrayMask;
-    }
+    } while (curProbe != loopIndex);
     return -1;
   }
 
@@ -102,7 +111,8 @@ public final class HashOperations {
    * This is a classical Knuth-style Open Addressing, Double Hash insert scheme for on-heap.
    * This method assumes that the input hash is not a duplicate.
    * Useful for rebuilding tables to avoid unnecessary comparisons.
-   * Returns the index of insertion, which is always positive or zero.
+   * Returns the index of insertion, which is always positive or zero. Throws an exception if the
+   * table is full with no empty slot.
    *
    * @param hashTable the hash table to insert into.
    * @param lgArrLongs <a href="{@docRoot}/resources/dictionary.html#lgArrLongs">See lgArrLongs</a>.
@@ -114,11 +124,17 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    while (hashTable[curProbe] != 0) {
+
+    final long loopIndex = curProbe;
+    do {
+      final long arrVal = hashTable[curProbe];
+      if (arrVal == EMPTY) {
+        hashTable[curProbe] = hash;
+        return curProbe;
+      }
       curProbe = (curProbe + stride) & arrayMask;
-    }
-    hashTable[curProbe] = hash;
-    return curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("No empty slot in table!");
   }
 
   /**
@@ -137,15 +153,20 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
+
     // search for duplicate or zero
-    while (hashTable[curProbe] != 0) {
-      if (hashTable[curProbe] == hash) { return curProbe; } // a duplicate
-      // not a duplicate and not zero, continue searching
+    final int loopIndex = curProbe;
+    do {
+      final long arrVal = hashTable[curProbe];
+      if (arrVal == EMPTY) {
+        hashTable[curProbe] = hash; // insert value
+        return ~curProbe;
+      } else if (arrVal == hash) {
+        return curProbe; // found a duplicate
+      }
       curProbe = (curProbe + stride) & arrayMask;
-    }
-    // must be zero, so insert
-    hashTable[curProbe] = hash;
-    return ~curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("Key not found and no empty slots!");
   }
 
   /**
@@ -200,14 +221,14 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1;
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-    long curArrayHash = mem.getLong(curProbeOffsetBytes);
-    while (curArrayHash != 0) {
-      if (curArrayHash == hash) { return curProbe; }
+    final int loopIndex = curProbe;
+    do {
+      final int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
+      final long curArrayHash = mem.getLong(curProbeOffsetBytes);
+      if (curArrayHash == EMPTY) { return -1; }
+      else if (curArrayHash == hash) { return curProbe; }
       curProbe = (curProbe + stride) & arrayMask;
-      curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-      curArrayHash = mem.getLong(curProbeOffsetBytes);
-    }
+    } while (curProbe != loopIndex);
     return -1;
   }
 
@@ -230,16 +251,18 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-    long curArrayHash = mem.getLong(curProbeOffsetBytes);
     // search for duplicate or zero
-    while (curArrayHash != 0L) {
+    final int loopIndex = curProbe;
+    do {
+      final int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
+      final long curArrayHash = mem.getLong(curProbeOffsetBytes);
+      if (curArrayHash == EMPTY) {
+        mem.putLong(curProbeOffsetBytes, hash);
+        return curProbe;
+      }
       curProbe = (curProbe + stride) & arrayMask;
-      curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-      curArrayHash = mem.getLong(curProbeOffsetBytes);
-    }
-    mem.putLong(curProbeOffsetBytes, hash);
-    return curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("No empty slot in table!");
   }
 
   /**
@@ -260,19 +283,18 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-    long curArrayHash = mem.getLong(curProbeOffsetBytes);
-    // search for duplicate or zero
-    while (curArrayHash != 0) {
-      if (curArrayHash == hash) { return curProbe; } // curArrayHash is a duplicate
+    final int loopIndex = curProbe;
+    do {
+      final int curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
+      final long curArrayHash = mem.getLong(curProbeOffsetBytes);
+      if (curArrayHash == EMPTY) {
+        mem.putLong(curProbeOffsetBytes, hash);
+        return ~curProbe;
+      } else if (curArrayHash == hash) { return curProbe; } // curArrayHash is a duplicate
       // curArrayHash is not a duplicate and not zero, continue searching
       curProbe = (curProbe + stride) & arrayMask;
-      curProbeOffsetBytes = (curProbe << 3) + memOffsetBytes;
-      curArrayHash = mem.getLong(curProbeOffsetBytes);
-    }
-    // must be zero, so insert
-    mem.putLong(curProbeOffsetBytes, hash);
-    return ~curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("Key not found and no empty slot in table!");
   }
 
   //FAST OFF-HEAP
@@ -298,17 +320,18 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    int curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
-    long curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
     // search for duplicate or zero
-    while (curArrayHash != 0L) {
+    final int loopIndex = curProbe;
+    do {
+      final int curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
+      final long curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
+      if (curArrayHash == EMPTY) {
+        unsafe.putLong(memObj, memAdd + curProbeOffsetBytes, hash);
+        return curProbe;
+      }
       curProbe = (curProbe + stride) & arrayMask;
-      curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
-      curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
-    }
-    // must be zero, so insert
-    unsafe.putLong(memObj, memAdd + curProbeOffsetBytes, hash);
-    return curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("No empty slot in table!");
   }
 
   /**
@@ -331,19 +354,19 @@ public final class HashOperations {
     final int arrayMask = (1 << lgArrLongs) - 1; // current Size -1
     final int stride = getStride(hash, lgArrLongs);
     int curProbe = (int) (hash & arrayMask);
-    int curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
-    long curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
     // search for duplicate or zero
-    while (curArrayHash != 0) {
-      if (curArrayHash == hash) { return curProbe; } // curArrayHash is a duplicate
+    final int loopIndex = curProbe;
+    do {
+      final int curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
+      final long curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
+      if (curArrayHash == EMPTY) {
+        unsafe.putLong(memObj, memAdd + curProbeOffsetBytes, hash);
+        return ~curProbe;
+      } else if (curArrayHash == hash) { return curProbe; } // curArrayHash is a duplicate
       // curArrayHash is not a duplicate and not zero, continue searching
       curProbe = (curProbe + stride) & arrayMask;
-      curProbeOffsetBytes = (curProbe << 3) + hashArrStartOffsetBytes;
-      curArrayHash = unsafe.getLong(memObj, memAdd + curProbeOffsetBytes);
-    }
-    // must be zero, so insert
-    unsafe.putLong(memObj, memAdd + curProbeOffsetBytes, hash);
-    return ~curProbe;
+    } while (curProbe != loopIndex);
+    throw new SketchesArgumentException("Key not found and no empty slot in table!");
   }
 
   /**
