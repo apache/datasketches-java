51fd2980998881522e9d076cc34b7a08695a45ee
lrhodes <lrhodes@yahoo-inc.com>
2017-10-29 10:32:10 -0700

    Update SingleItem Sketch

---
+++ src/test/java/com/yahoo/sketches/theta/SingleItemSketchTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017, Yahoo! Inc. Licensed under the terms of the
+ * Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.sketches.theta;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Lee Rhodes
+ */
+public class SingleItemSketchTest {
+
+  @Test
+  public void check1() {
+
+  }
+
+}
