85c69382c9f1c52dfad1dc85e497b8734c9daa75
eshcar <eshcar@oath.com>
2018-11-18 20:59:01 +0200

    update copyright to 2018

--- src/main/java/com/yahoo/sketches/theta/SharedThetaSketch.java
+++ src/main/java/com/yahoo/sketches/theta/SharedThetaSketch.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018, Yahoo! Inc. Licensed under the terms of the
+ * Apache License 2.0. See LICENSE file at the project root for terms.
+ */
 package com.yahoo.sketches.theta;
 
 import java.util.concurrent.atomic.AtomicBoolean;
