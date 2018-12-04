9777619b6a7fd39ee56e11e12eba11b0f386f850
lrhodes <lrhodes@yahoo-inc.com>
2018-12-04 11:02:40 -0800

    Simplify xxHash testing in core.

--- src/main/java/com/yahoo/sketches/hash/XxHash.java
+++ src/main/java/com/yahoo/sketches/hash/XxHash.java
@@ -33,6 +33,12 @@ public class XxHash {
   private static final long P4 = -8796714831421723037L;
   private static final long P5 =  2870177450012600261L;
 
+
+  public static long hash(final Memory mem, final long offsetBytes, final long lengthBytes,
+      final long seed) {
+    return mem.xxHash64(offsetBytes, lengthBytes, seed);
+  }
+
   /**
    * Returns a 64-bit hash.
    * @param in a long
@@ -48,109 +54,6 @@ public class XxHash {
     k1 *= P1;
     hash ^= k1;
     hash = (Long.rotateLeft(hash, 27) * P1) + P4;
-    return finalize(hash);
-  }
-
-  /**
-   * Returns a 64-bit hash.
-   *
-   * @param mem The source Memory
-   * @param offsetBytes the offset in bytes
-   * @param lengthBytes the length in bytes
-   * @param seed a given seed
-   * @return a 64-bit hash
-   */
-  public static long hash(final Memory mem, long offsetBytes, final long lengthBytes, final long seed) {
-    long hash;
-    long remaining = lengthBytes;
-
-    if (remaining >= 32) {
-      long v1 = seed + P1 + P2;
-      long v2 = seed + P2;
-      long v3 = seed;
-      long v4 = seed - P1;
-
-      do {
-        v1 += mem.getLong(offsetBytes) * P2;
-        v1 = Long.rotateLeft(v1, 31);
-        v1 *= P1;
-
-        v2 += mem.getLong(offsetBytes + 8L) * P2;
-        v2 = Long.rotateLeft(v2, 31);
-        v2 *= P1;
-
-        v3 += mem.getLong(offsetBytes + 16L) * P2;
-        v3 = Long.rotateLeft(v3, 31);
-        v3 *= P1;
-
-        v4 += mem.getLong(offsetBytes + 24L) * P2;
-        v4 = Long.rotateLeft(v4, 31);
-        v4 *= P1;
-
-        offsetBytes += 32;
-        remaining -= 32;
-      } while (remaining >= 32);
-
-      hash = Long.rotateLeft(v1, 1)
-          + Long.rotateLeft(v2, 7)
-          + Long.rotateLeft(v3, 12)
-          + Long.rotateLeft(v4, 18);
-
-      v1 *= P2;
-      v1 = Long.rotateLeft(v1, 31);
-      v1 *= P1;
-      hash ^= v1;
-      hash = (hash * P1) + P4;
-
-      v2 *= P2;
-      v2 = Long.rotateLeft(v2, 31);
-      v2 *= P1;
-      hash ^= v2;
-      hash = (hash * P1) + P4;
-
-      v3 *= P2;
-      v3 = Long.rotateLeft(v3, 31);
-      v3 *= P1;
-      hash ^= v3;
-      hash = (hash * P1) + P4;
-
-      v4 *= P2;
-      v4 = Long.rotateLeft(v4, 31);
-      v4 *= P1;
-      hash ^= v4;
-      hash = (hash * P1) + P4;
-    } //end remaining >= 32
-    else {
-      hash = seed + P5;
-    }
-
-    hash += lengthBytes;
-
-    while (remaining >= 8) {
-      long k1 = mem.getLong(offsetBytes);
-      k1 *= P2;
-      k1 = Long.rotateLeft(k1, 31);
-      k1 *= P1;
-      hash ^= k1;
-      hash = (Long.rotateLeft(hash, 27) * P1) + P4;
-      offsetBytes += 8;
-      remaining -= 8;
-    }
-
-    if (remaining >= 4) { //treat as unsigned ints
-      hash ^= (mem.getInt(offsetBytes) & 0XFFFF_FFFFL) * P1;
-      hash = (Long.rotateLeft(hash, 23) * P2) + P3;
-      offsetBytes += 4;
-      remaining -= 4;
-    }
-
-    while (remaining != 0) { //treat as unsigned bytes
-      hash ^= (mem.getByte(offsetBytes) & 0XFFL) * P5;
-      hash = Long.rotateLeft(hash, 11) * P1;
-      --remaining;
-      ++offsetBytes;
-    }
-
     return finalize(hash);
   }
 
--- src/test/java/com/yahoo/sketches/hash/XxHashTest.java
+++ src/test/java/com/yahoo/sketches/hash/XxHashTest.java
@@ -6,12 +6,10 @@
 package com.yahoo.sketches.hash;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
 
-import com.yahoo.memory.WritableMemory;
-
+import com.yahoo.memory.Memory;
 
 /**
  * @author Lee Rhodes
@@ -19,65 +17,14 @@ import com.yahoo.memory.WritableMemory;
 public class XxHashTest {
 
   @Test
-  public void offsetChecks() {
-    long seed = 12345;
-    int blocks = 6;
-    int cap = blocks * 16;
-
-    long hash;
-
-    WritableMemory wmem = WritableMemory.allocate(cap);
-    for (int i = 0; i < cap; i++) { wmem.putByte(i, (byte)(-128 + i)); }
-
-    for (int offset = 0; offset < 16; offset++) {
-      int arrLen = cap - offset;
-      hash = XxHash.hash(wmem, offset, arrLen, seed);
-      assertTrue(hash != 0);
-    }
-  }
-
-  @Test
-  public void byteArrChecks() {
-    long seed = 0;
-    int offset = 0;
-    int bytes = 16;
-
-    for (int j = 1; j < bytes; j++) {
-      byte[] in = new byte[bytes];
-
-      WritableMemory wmem = WritableMemory.wrap(in);
-      for (int i = 0; i < j; i++) { wmem.putByte(i, (byte) (-128 + i)); }
-
-      long hash = XxHash.hash(wmem, offset, bytes, seed);
-      assertTrue(hash != 0);
-    }
-  }
-
-  @Test
   public void longCheck() {
     long seed = 0;
-    long hash = XxHash.hash(123L, seed);
-    assertTrue(hash != 0);
-  }
-
-  @Test
-  public void collisionTest() {
-    WritableMemory wmem = WritableMemory.allocate(128);
-    wmem.putLong(0, 1);
-    wmem.putLong(16, 42);
-    wmem.putLong(32, 2);
-    long h1 = XxHash.hash(wmem, 0, wmem.getCapacity(), 0);
-
-    wmem.putLong(0, 1 + 0xBA79078168D4BAFL);
-    wmem.putLong(32, 2 + 0x9C90005B80000000L);
-    long h2 = XxHash.hash(wmem, 0, wmem.getCapacity(), 0);
-    assertEquals(h1, h2);
-
-    wmem.putLong(0, 1 + (0xBA79078168D4BAFL * 2));
-    wmem.putLong(32, 2 + (0x9C90005B80000000L * 2));
-
-    long h3 = XxHash.hash(wmem, 0, wmem.getCapacity(), 0);
-    assertEquals(h2, h3);
+    long hash1 = XxHash.hash(123L, seed);
+    long[] arr = new long[1];
+    arr[0] = 123L;
+    Memory mem = Memory.wrap(arr);
+    long hash2 = XxHash.hash(mem, 0, 8, 0);
+    assertEquals(hash2, hash1);
   }
 
 }
