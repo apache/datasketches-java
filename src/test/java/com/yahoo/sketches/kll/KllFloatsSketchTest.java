2fa1c9155b9c29ff6a6e37628b474baa694504c2
saydakov <saydakov@yahoo-inc.com>
2018-12-18 15:48:25 -0800

    test getQuantiles(numEvenlySpaced)

--- src/test/java/com/yahoo/sketches/kll/KllFloatsSketchTest.java
+++ src/test/java/com/yahoo/sketches/kll/KllFloatsSketchTest.java
@@ -383,4 +383,18 @@ public class KllFloatsSketchTest {
     assertEquals(lvlCap, 8);
   }
 
+  @Test
+  public void getQuantiles() {
+    final KllFloatsSketch sketch = new KllFloatsSketch();
+    sketch.update(1);
+    sketch.update(2);
+    sketch.update(3);
+    final float[] quantiles1 = sketch.getQuantiles(new double[] {0, 0.5, 1}); 
+    final float[] quantiles2 = sketch.getQuantiles(3);
+    assertEquals(quantiles1, quantiles2);
+    assertEquals(quantiles1[0], 1f);
+    assertEquals(quantiles1[1], 2f);
+    assertEquals(quantiles1[2], 3f);
+  }
+
 }
