75714f17625358e3d2e59da63d1089be53840490
lrhodes <lrhodes@yahoo-inc.com>
2019-02-21 12:59:41 -0800

    Minor code cleanup.

--- src/main/java/com/yahoo/sketches/theta/ConcurrentPropagationService.java
+++ src/main/java/com/yahoo/sketches/theta/ConcurrentPropagationService.java
@@ -13,25 +13,13 @@ final class ConcurrentPropagationService {
   // Singleton
   private static volatile ConcurrentPropagationService instance = null;
   private static ExecutorService[] propagationExecutorService = null;
-  //
   static int NUM_POOL_THREADS = 3; // Default: 3 threads
 
-  public static ExecutorService getExecutorService(final long id) {
-    return getInstance().initExecutorService((int) id % NUM_POOL_THREADS);
-  }
-
-  @SuppressWarnings("static-access")
-  public static ExecutorService resetExecutorService(final long id) {
-    return getInstance().propagationExecutorService[(int) id % NUM_POOL_THREADS] = null;
-  }
-
-  //make the constructor private so that this class cannot be
-  //instantiated externally
   private ConcurrentPropagationService() {
     propagationExecutorService = new ExecutorService[NUM_POOL_THREADS];
   }
 
-  //Get the only object available
+  //Factory: Get the singleton
   private static ConcurrentPropagationService getInstance() {
     if (instance == null) {
       synchronized (ConcurrentPropagationService.class) {
@@ -41,6 +29,15 @@ final class ConcurrentPropagationService {
       }
     }
     return instance;
+  }
+
+  public static ExecutorService getExecutorService(final long id) {
+    return getInstance().initExecutorService((int) id % NUM_POOL_THREADS);
+  }
+
+  @SuppressWarnings("static-access")
+  public static ExecutorService resetExecutorService(final long id) {
+    return getInstance().propagationExecutorService[(int) id % NUM_POOL_THREADS] = null;
   }
 
   @SuppressWarnings("static-method")
