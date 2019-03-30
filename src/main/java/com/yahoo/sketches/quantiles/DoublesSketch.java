71ae7f3dec485dec8aaa76ef15f1aaf7638d5693
lrhodes <lrhodes@yahoo-inc.com>
2019-03-30 14:15:10 -0700

    Added unit test, made DoublesSketch.rand modifiable.

--- src/main/java/com/yahoo/sketches/quantiles/DoublesSketch.java
+++ src/main/java/com/yahoo/sketches/quantiles/DoublesSketch.java
@@ -138,7 +138,7 @@ public abstract class DoublesSketch {
    * received in exactly the same order. This is only useful when performing test comparisons,
    * otherwise is not recommended.
    */
-  public static final Random rand = new Random(1);
+  public static Random rand = new Random();
 
   DoublesSketch(final int k) {
     Util.checkK(k);
--- src/test/java/com/yahoo/sketches/quantiles/DebugUnionTest.java
+++ src/test/java/com/yahoo/sketches/quantiles/DebugUnionTest.java
@@ -6,8 +6,11 @@
 package com.yahoo.sketches.quantiles;
 
 import static com.yahoo.sketches.quantiles.Util.LS;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.util.HashSet;
+import java.util.Random;
 
 import org.testng.annotations.Test;
 
@@ -19,49 +22,83 @@ import com.yahoo.memory.WritableMemory;
  */
 public class DebugUnionTest {
 
-  @SuppressWarnings("resource")
   @Test
   public void test() {
-    //final Random rnd = new Random(1);
     final int n = 70_000;
+    final int valueLimit = 1000;
     final int numSketches = 3;
-    final int bytes = 10_000_000;
-    WritableDirectHandle wdh = WritableMemory.allocateDirect(bytes);
-    WritableMemory wmem = wdh.get();
-    final DoublesUnion union = DoublesUnion.builder().setMaxK(8).build(wmem);
-    //final DoublesUnion union = DoublesUnion.builder().build();
-    final HashSet<Double> set = new HashSet<>();
-    {
-      for (int s = 0; s < numSketches; s++) {
-        final UpdateDoublesSketch updateSketch = DoublesSketch.builder().setK(8).build();
-        for (int i = 0; i < n; i++) {
-          final double value = DoublesSketch.rand.nextInt(1000) + 1;
-          updateSketch.update(value);
-          set.add(value);
-        }
-        union.update(updateSketch);
-        println(updateSketch.toString(true, true));
-      }
-    }
-    DoublesSketch sketch = union.getResult();
-    //double[] quantiles = sketch.getQuantiles(1000);
-    int count = 0;
-    int errors = 0;
-    //DoublesSketchIterator it = sketch.iterator();
-    DoublesSketchIterator it = ((DoublesUnionImplR)union).gadget_.iterator();
-    while (it.next()) {
-      count++;
-      if (!set.contains(it.getValue())) {
-        errors++;
-        println("Weight: " + it.getWeight());
-      }
+    final int sketchK = 8;
+    final int unionK = 8;
+    UpdateDoublesSketch[] sketchArr = new UpdateDoublesSketch[numSketches];
+
+    //builds the input sketches, all on heap
+    DoublesSketch.rand = new Random(1); //make deterministic for test
+    final HashSet<Double> set = new HashSet<>(); //holds input values
+    for (int s = 0; s < numSketches; s++) {
+      sketchArr[s] = buildHeapSketch(sketchK, n, valueLimit, set);
     }
 
-    println("Values: " + count + ", errors: " + errors);
-    //assertEquals(count, 0);
-    //System.out.println(sketch.toString());
-    println(sketch.toString(true, true));
-    wdh.close();
+    //loads the on heap union
+    DoublesSketch.rand = new Random(1); //make deterministic for test
+    DoublesUnion hUnion = DoublesUnion.builder().setMaxK(unionK).build();
+    for (int s = 0; s < numSketches; s++) { hUnion.update(sketchArr[s]); }
+    DoublesSketch hSketch = hUnion.getResult();
+
+    //loads the direct union
+    DoublesSketch.rand = new Random(1); //make deterministic for test
+    DoublesUnion dUnion;
+    DoublesSketch dSketch;
+    try ( WritableDirectHandle wdh = WritableMemory.allocateDirect(10_000_000) ) {
+      WritableMemory wmem = wdh.get();
+      dUnion = DoublesUnion.builder().setMaxK(8).build(wmem);
+      for (int s = 0; s < numSketches; s++) { dUnion.update(sketchArr[s]); }
+      dSketch = dUnion.getResult(); //result is on heap
+    }
+
+    //iterates and counts errors
+    int hCount = hSketch.getRetainedItems();
+    int dCount = dSketch.getRetainedItems();
+
+    assertEquals(hCount, dCount); //Retained items must be the same
+
+    double[] heapItems = new double[hCount];
+    double[] directItems = new double[dCount];
+    int hErrors = 0;
+    int dErrors = 0;
+
+    DoublesSketchIterator hit = hSketch.iterator();
+    DoublesSketchIterator dit = dSketch.iterator();
+    int i = 0;
+    while (hit.next() && dit.next()) {
+      double v = hit.getValue();
+      heapItems[i] = v;
+      if (!set.contains(v)) { hErrors++; }
+
+      double w = dit.getValue();
+      directItems[i] = w;
+      if (!set.contains(w)) { dErrors++; }
+      i++;
+      assertEquals(v, w, 0); //Items must be returned in same order and be equal
+    }
+    assertTrue(hErrors == 0);
+    assertTrue(dErrors == 0);
+
+    println("HeapUnion  : Values: " + hCount + ", errors: " + hErrors);
+    //println(hSketch.toString(true, true));
+
+    println("DirectUnion: Values: " + dCount + ", errors: " + dErrors);
+    //println(dSketch.toString(true, true));
+  }
+
+  private static UpdateDoublesSketch buildHeapSketch(final int k, final int n, final int valueLimit,
+      final HashSet<Double> set) {
+    final UpdateDoublesSketch uSk = DoublesSketch.builder().setK(k).build();
+    for (int i = 0; i < n; i++) {
+      final double value = DoublesSketch.rand.nextInt(valueLimit) + 1;
+      uSk.update(value);
+      set.add(value);
+    }
+    return uSk;
   }
 
   @Test
