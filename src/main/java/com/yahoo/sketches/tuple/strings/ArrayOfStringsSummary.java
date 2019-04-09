564769aa40cff66d9b5700b31603f7cb293dd578
Lee Rhodes <leerho@users.noreply.github.com>
2019-04-09 16:52:46 -0700

    Removed unneeded method.

--- src/main/java/com/yahoo/sketches/tuple/strings/ArrayOfStringsSummary.java
+++ src/main/java/com/yahoo/sketches/tuple/strings/ArrayOfStringsSummary.java
@@ -139,20 +139,6 @@ public class ArrayOfStringsSummary implements UpdatableSummary<String[]> {
     return sb.toString();
   }
 
-  /**
-   * @param key to be hashed
-   * @return an index into the hash table This hash function is taken from the internals of
-   * Austin Appleby's MurmurHash3 algorithm. It is also used by the Trove for Java libraries.
-   */
-  static long hash(long key) {
-    key ^= key >>> 33;
-    key *= 0xff51afd7ed558ccdL;
-    key ^= key >>> 33;
-    key *= 0xc4ceb9fe1a85ec53L;
-    key ^= key >>> 33;
-    return key;
-  }
-
   static void checkNumNodes(final int numNodes) {
     if (numNodes > 127)  {
       throw new SketchesArgumentException("Number of nodes cannot exceed 127.");
