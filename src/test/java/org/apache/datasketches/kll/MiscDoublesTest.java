0f0f0757517db6cb0d8fa399b079e8d590d6f4e9
Lee Rhodes <leerho@users.noreply.github.com>
2022-02-15 13:47:42 -0800

    Update unit tests for KllDoubles and KllFloats

--- src/test/java/org/apache/datasketches/kll/MiscDoublesTest.java
+++ src/test/java/org/apache/datasketches/kll/MiscDoublesTest.java
@@ -20,7 +20,12 @@
 package org.apache.datasketches.kll;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import java.util.Objects;
+
+import org.apache.datasketches.SketchesArgumentException;
+import org.apache.datasketches.memory.WritableMemory;
 import org.testng.annotations.Test;
 
 /**
@@ -55,6 +60,63 @@ public class MiscDoublesTest {
     println("Ext     : " + est);
     println("UB      : " + ub);
     println("LB      : " + lb);
+  }
+
+  @Test(expectedExceptions = SketchesArgumentException.class)
+  public void checkHeapifyExceptions1() {
+    KllDoublesSketch sk = new KllDoublesSketch();
+    WritableMemory wmem = WritableMemory.writableWrap(sk.toByteArray());
+    wmem.putByte(6, (byte)4); //corrupt M
+    KllDoublesSketch.heapify(wmem);
+  }
+
+  @Test(expectedExceptions = SketchesArgumentException.class)
+  public void checkHeapifyExceptions2() {
+    KllDoublesSketch sk = new KllDoublesSketch();
+    WritableMemory wmem = WritableMemory.writableWrap(sk.toByteArray());
+    wmem.putByte(0, (byte)1); //corrupt preamble ints, should be 2
+    KllDoublesSketch.heapify(wmem);
+  }
+
+  @Test(expectedExceptions = SketchesArgumentException.class)
+  public void checkHeapifyExceptions3() {
+    KllDoublesSketch sk = new KllDoublesSketch();
+    sk.update(1.0f);
+    sk.update(2.0f);
+    WritableMemory wmem = WritableMemory.writableWrap(sk.toByteArray());
+    wmem.putByte(0, (byte)1); //corrupt preamble ints, should be 5
+    KllDoublesSketch.heapify(wmem);
+  }
+
+  @Test(expectedExceptions = SketchesArgumentException.class)
+  public void checkHeapifyExceptions4() {
+    KllDoublesSketch sk = new KllDoublesSketch();
+    WritableMemory wmem = WritableMemory.writableWrap(sk.toByteArray());
+    wmem.putByte(1, (byte)0); //corrupt SerVer, should be 1 or 2
+    KllDoublesSketch.heapify(wmem);
+  }
+
+  @Test(expectedExceptions = SketchesArgumentException.class)
+  public void checkHeapifyExceptions5() {
+    KllDoublesSketch sk = new KllDoublesSketch();
+    WritableMemory wmem = WritableMemory.writableWrap(sk.toByteArray());
+    wmem.putByte(2, (byte)0); //corrupt FamilyID, should be 15
+    KllDoublesSketch.heapify(wmem);
+  }
+
+  @Test
+  public void checkMisc() {
+    KllDoublesSketch sk = new KllDoublesSketch(8, true);
+    assertTrue(Objects.isNull(sk.getQuantiles(10)));
+    sk.toString(true, true);
+    for (int i = 0; i < 20; i++) { sk.update(i); }
+    sk.toString(true, true);
+    sk.toByteArray();
+    final double[] items = sk.getItems();
+    assertEquals(items.length, 16);
+    final int[] levels = sk.getLevels();
+    assertEquals(levels.length, 3);
+    assertEquals(sk.getNumLevels(), 2);
   }
 
   //@Test //requires visual check
