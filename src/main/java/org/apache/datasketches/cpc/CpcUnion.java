c95e30dff9e211b7b0b4313c6c650449b403c7b8
Lee Rhodes <leerho@users.noreply.github.com>
2022-12-09 18:27:35 -0800

    Had to do some workarounds to prevent CodeQL from creating warnings that block successful tests.
    There is no suppression capability yet for CodeQL

--- src/main/java/org/apache/datasketches/cpc/CpcUnion.java
+++ src/main/java/org/apache/datasketches/cpc/CpcUnion.java
@@ -298,6 +298,10 @@ public class CpcUnion {
     final int state = ((sourceFlavorOrd - 1) << 1) | ((union.bitMatrix != null) ? 1 : 0);
     switch (state) {
       case 0 : { //A: Sparse, bitMatrix == null, accumulator valid
+        if (union.accumulator == null) {
+          //CodeQL could not figure this out so I have to insert this.
+          throw new SketchesStateException("union.accumulator can never be null here.");
+        }
         if ((union.accumulator.getFlavor() == EMPTY)
             && (union.lgK == source.lgK)) {
           union.accumulator = source.copy();
