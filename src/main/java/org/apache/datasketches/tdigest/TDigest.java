0ea8b3de6482e6f1c6c845d2e8ddb697c2b0c10e
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2024-02-14 19:32:08 -0800

    merge, getRank, getQuantile

--- src/main/java/org/apache/datasketches/tdigest/TDigest.java
+++ src/main/java/org/apache/datasketches/tdigest/TDigest.java
@@ -46,7 +46,7 @@ public final class TDigest {
   private int numCentroids_;
   private double[] centroidMeans_;
   private long[] centroidWeights_;
-  private long totalWeight_;
+  private long centroidsWeight_;
   private int bufferCapacity_;
   private int numBuffered_;
   private double[] bufferValues_;
@@ -82,7 +82,7 @@ public final class TDigest {
     bufferWeights_ = new long[bufferCapacity_];
     numCentroids_ = 0;
     numBuffered_ = 0;
-    totalWeight_ = 0;
+    centroidsWeight_ = 0;
     bufferedWeight_ = 0;
   }
 
@@ -102,6 +102,31 @@ public final class TDigest {
   }
 
   public void merge(final TDigest other) {
+    if (other.isEmpty()) return;
+    int num = numCentroids_ + numBuffered_ + other.numCentroids_ + other.numBuffered_;
+    if (num <= bufferCapacity_) {
+      System.arraycopy(other.bufferValues_, 0, bufferValues_, numBuffered_, other.numBuffered_);
+      System.arraycopy(other.bufferWeights_, 0, bufferWeights_, numBuffered_, other.numBuffered_);
+      numBuffered_ += other.numBuffered_;
+      System.arraycopy(other.centroidMeans_, 0, bufferValues_, numBuffered_, other.numCentroids_);
+      System.arraycopy(other.centroidWeights_, 0, bufferWeights_, numBuffered_, other.numCentroids_);
+      numBuffered_ += other.numCentroids_;
+      bufferedWeight_ += other.getTotalWeight();
+      minValue_ = Math.min(minValue_, other.minValue_);
+      maxValue_ = Math.max(maxValue_, other.maxValue_);
+    } else {
+      final double[] values = new double[num];
+      final long[] weights = new long[num];
+      System.arraycopy(bufferValues_, 0, values, 0, numBuffered_);
+      System.arraycopy(bufferWeights_, 0, weights, 0, numBuffered_);
+      System.arraycopy(other.bufferValues_, 0, values, numBuffered_, other.numBuffered_);
+      System.arraycopy(other.bufferWeights_, 0, weights, numBuffered_, other.numBuffered_);
+      numBuffered_ += other.numBuffered_;
+      System.arraycopy(other.centroidMeans_, 0, values, numBuffered_, other.numCentroids_);
+      System.arraycopy(other.centroidWeights_, 0, weights, numBuffered_, other.numCentroids_);
+      numBuffered_ += other.numCentroids_;
+      merge(values, weights, bufferedWeight_ + other.centroidsWeight_, numBuffered_);
+    }
   }
 
   public void compress() {
@@ -123,17 +148,107 @@ public final class TDigest {
   }
 
   public long getTotalWeight() {
-    return totalWeight_ + bufferedWeight_;
+    return centroidsWeight_ + bufferedWeight_;
   }
 
-  public double getRank(final double quantile) {
-    if (isEmpty()) { throw new IllegalArgumentException(QuantilesAPI.EMPTY_MSG); }
-    return 0;
+  public double getRank(final double value) {
+    if (isEmpty()) throw new IllegalArgumentException(QuantilesAPI.EMPTY_MSG);
+    if (Double.isNaN(value)) throw new IllegalArgumentException("Operation is undefined for Nan");
+    if (value < minValue_) return 0;
+    if (value > maxValue_) return 1;
+    if (numCentroids_ + numBuffered_ == 1) return 0.5;
+
+    mergeBuffered(); // side effect
+
+    // left tail
+    final double firstMean = centroidMeans_[0];
+    if (value < firstMean) {
+      if (firstMean - minValue_ > 0) {
+        if (value == minValue_) return 0.5 / centroidsWeight_;
+        return (1.0 + (value - minValue_) / (firstMean - minValue_) * (centroidWeights_[0] / 2.0 - 1.0));
+      }
+      return 0; // should never happen
+    }
+
+    // right tail
+    final double lastMean = centroidMeans_[numCentroids_ - 1];
+    if (value > lastMean) {
+      if (maxValue_ - lastMean > 0) {
+        if (value == maxValue_) return 1.0 - 0.5 / centroidsWeight_;
+        return 1.0 - ((1.0 + (maxValue_ - value) / (maxValue_ - lastMean) * (centroidWeights_[numCentroids_ - 1] / 2.0 - 1.0)) / centroidsWeight_);
+      }
+      return 1; // should never happen
+    }
+
+    int lower = BinarySearch.lowerBound(centroidMeans_, 0, numCentroids_, value);
+    if (lower == numCentroids_) throw new RuntimeException("lower == end in getRank()");
+    int upper = BinarySearch.upperBound(centroidMeans_, lower, numCentroids_, value);
+    if (upper == 0) throw new RuntimeException("upper == begin in getRank()");
+    if (value < centroidMeans_[lower]) lower--;
+    if (upper == numCentroids_ || !(centroidMeans_[upper - 1] < value)) upper--;
+
+    double weightBelow = 0;
+    int i = 0;
+    while (i != lower) weightBelow += centroidWeights_[i++];
+    weightBelow += centroidWeights_[lower] / 2.0;
+
+    double weightDelta = 0;
+    while (i != upper) weightDelta += centroidWeights_[i++];
+    weightDelta -= centroidWeights_[lower] / 2.0;
+    weightDelta += centroidWeights_[upper] / 2.0;
+    if (centroidMeans_[upper] - centroidMeans_[lower] > 0) {
+      return (weightBelow + weightDelta * (value - centroidMeans_[lower]) / (centroidMeans_[upper] - centroidMeans_[lower])) / centroidsWeight_;
+    }
+    return (weightBelow + weightDelta / 2.0) / centroidsWeight_;
   }
 
   public double getQuantile(final double rank) {
-    if (isEmpty()) { throw new IllegalArgumentException(QuantilesAPI.EMPTY_MSG); }
-    return 0;
+    if (isEmpty()) throw new IllegalArgumentException(QuantilesAPI.EMPTY_MSG);
+    if (Double.isNaN(rank)) throw new IllegalArgumentException("Operation is undefined for Nan");
+    if (rank < 0 || rank > 1) throw new IllegalArgumentException("Normalized rank must be within [0, 1]"); 
+    
+    mergeBuffered(); // side effect
+
+    if (numCentroids_ == 1) return centroidMeans_[0];
+
+    // at least 2 centroids
+    final double weight = rank * centroidsWeight_;
+    if (weight < 1) return minValue_;
+    if (weight > centroidsWeight_ - 1.0) return maxValue_;
+    final double firstWeight = centroidWeights_[0];
+    if (firstWeight > 1 && weight < firstWeight / 2.0) {
+      return minValue_ + (weight - 1.0) / (firstWeight / 2.0 - 1.0) * (centroidMeans_[0] - minValue_);
+    }
+    final double lastWeight = centroidWeights_[numCentroids_ - 1];
+    if (lastWeight > 1 && centroidsWeight_ - weight <= lastWeight / 2.0) {
+      return maxValue_ + (centroidsWeight_ - weight - 1.0) / (lastWeight / 2.0 - 1.0) * (maxValue_ - centroidMeans_[numCentroids_ - 1]);
+    }
+
+    // interpolate between extremes
+    double weightSoFar = firstWeight / 2.0;
+    for (int i = 0; i < numCentroids_ - 1; i++) {
+      final double dw = (centroidWeights_[i] + centroidWeights_[i + 1]) / 2.0;
+      if (weightSoFar + dw > weight) {
+        // the target weight is between centroids i and i+1
+        double leftWeight = 0;
+        if (centroidWeights_[i] == 1) {
+          if (weight - weightSoFar < 0.5) return centroidMeans_[i];
+          leftWeight = 0.5;
+        }
+        double rightWeight = 0;
+        if (centroidWeights_[i + 1] == 1) {
+          if (weightSoFar + dw - weight <= 0.5) return centroidMeans_[i + 1];
+          rightWeight = 0.5;
+        }
+        final double w1 = weight - weightSoFar - leftWeight;
+        final double w2 = weightSoFar + dw - weight - rightWeight;
+        return weightedAverage(centroidMeans_[i], w1, centroidMeans_[i + 1], w2);
+      }
+      weightSoFar += dw;
+    }
+    final double w1 = weight - centroidsWeight_ - centroidWeights_[numCentroids_ - 1] / 2.0;
+    final double w2 = centroidWeights_[numCentroids_ - 1] / 2.0 - w1;
+    return weightedAverage(centroidWeights_[numCentroids_ - 1], w1, maxValue_, w2);
   }
 
   /**
@@ -161,8 +276,9 @@ public final class TDigest {
         + " Buffered: " + numBuffered_ + "\n"
         + " Centroids Capacity: " + centroidsCapacity_ + "\n"
         + " Buffer Capacity: " + bufferCapacity_ + "\n"
-        + " Total Weight: " + totalWeight_ + "\n"
-        + " Unmerged Weight: " + bufferedWeight_ + "\n"
+        + " Centroids Weight: " + centroidsWeight_ + "\n"
+        + " Buffered Weight: " + bufferedWeight_ + "\n"
+        + " Total Weight: " + getTotalWeight() + "\n"
         + " Reverse Merge: " + reverseMerge_ + "\n";
     if (!isEmpty()) {
       str += " Min: " + minValue_ + "\n"
@@ -187,48 +303,53 @@ public final class TDigest {
 
   private void mergeBuffered() {
     if (numBuffered_ == 0) return;
+    merge(bufferValues_, bufferWeights_, bufferedWeight_, numBuffered_);
+  }
+
+  // assumes that there is enough room in the input arrays to add centroids from this TDigest
+  private void merge(final double[] values, final long[] weights, final long weight, int num) {
     final boolean reverse = USE_ALTERNATING_SORT & reverseMerge_;
-    System.arraycopy(centroidMeans_, 0, bufferValues_, numBuffered_, numCentroids_);
-    System.arraycopy(centroidWeights_, 0, bufferWeights_, numBuffered_, numCentroids_);
-    numBuffered_ += numCentroids_;
-    totalWeight_ += bufferedWeight_;
+    System.arraycopy(centroidMeans_, 0, values, num, numCentroids_);
+    System.arraycopy(centroidWeights_, 0, weights, num, numCentroids_);
+    num += numCentroids_;
+    centroidsWeight_ += weight;
     numCentroids_ = 0;
-    Sort.stableSort(bufferValues_, bufferWeights_, numBuffered_);
-    if (reverse) {
-      Sort.reverse(bufferValues_, numBuffered_);
-      Sort.reverse(bufferWeights_, numBuffered_);
+    Sort.stableSort(values, weights, num);
+    if (reverse) { // this might be avoidable if stableSort could be implemented with a boolean parameter to invert the logic
+      Sort.reverse(values, num);
+      Sort.reverse(weights, num);
     }
-    centroidMeans_[0] = bufferValues_[0];
-    centroidWeights_[0] = bufferWeights_[0];
+    centroidMeans_[0] = values[0];
+    centroidWeights_[0] = weights[0];
     numCentroids_++;
     int current = 1;
     double weightSoFar = 0;
-    final double normalizer = ScaleFunction.normalizer(internalK_, totalWeight_);
+    final double normalizer = ScaleFunction.normalizer(internalK_, centroidsWeight_);
     double k1 = ScaleFunction.k(0, normalizer);
-    double wLimit = totalWeight_ * ScaleFunction.q(k1 + 1, normalizer);
-    while (current != numBuffered_) {
-      final double proposedWeight = centroidWeights_[numCentroids_ - 1] + bufferWeights_[current];
+    double wLimit = centroidsWeight_ * ScaleFunction.q(k1 + 1, normalizer);
+    while (current != num) {
+      final double proposedWeight = centroidWeights_[numCentroids_ - 1] + weights[current];
       final boolean addThis;
-      if (current == 1 || current == numBuffered_ - 1) {
+      if (current == 1 || current == num - 1) {
         addThis = false;
       } else if (USE_WEIGHT_LIMIT) {
-        final double q0 = weightSoFar / totalWeight_;
-        final double q2 = (weightSoFar + proposedWeight) / totalWeight_;
-        addThis = proposedWeight <= totalWeight_ * Math.min(ScaleFunction.max(q0, normalizer), ScaleFunction.max(q2, normalizer));
+        final double q0 = weightSoFar / centroidsWeight_;
+        final double q2 = (weightSoFar + proposedWeight) / centroidsWeight_;
+        addThis = proposedWeight <= centroidsWeight_ * Math.min(ScaleFunction.max(q0, normalizer), ScaleFunction.max(q2, normalizer));
       } else {
         addThis = weightSoFar + proposedWeight <= wLimit;
       }
       if (addThis) { // merge into existing centroid
-        centroidWeights_[numCentroids_ - 1] += bufferWeights_[current];
-        centroidMeans_[numCentroids_ - 1] += (bufferValues_[current] - centroidMeans_[numCentroids_ - 1]) * bufferWeights_[current] / centroidWeights_[numCentroids_ - 1];
+        centroidWeights_[numCentroids_ - 1] += weights[current];
+        centroidMeans_[numCentroids_ - 1] += (values[current] - centroidMeans_[numCentroids_ - 1]) * weights[current] / centroidWeights_[numCentroids_ - 1];
       } else { // copy to a new centroid
         weightSoFar += centroidWeights_[numCentroids_ - 1];
         if (!USE_WEIGHT_LIMIT) {
-          k1 = ScaleFunction.k(weightSoFar / totalWeight_, normalizer);
-          wLimit = totalWeight_ * ScaleFunction.q(k1 + 1, normalizer);
+          k1 = ScaleFunction.k(weightSoFar / centroidsWeight_, normalizer);
+          wLimit = centroidsWeight_ * ScaleFunction.q(k1 + 1, normalizer);
         }
-        centroidMeans_[numCentroids_] = bufferValues_[current];
-        centroidWeights_[numCentroids_] = bufferWeights_[current];
+        centroidMeans_[numCentroids_] = values[current];
+        centroidWeights_[numCentroids_] = weights[current];
         numCentroids_++;
       }
       current++;
@@ -237,9 +358,9 @@ public final class TDigest {
       Sort.reverse(centroidMeans_, numCentroids_);
       Sort.reverse(centroidWeights_, numCentroids_);
     }
-    reverseMerge_ = !reverseMerge_;
     numBuffered_ = 0;
     bufferedWeight_ = 0;
+    reverseMerge_ = !reverseMerge_;
     minValue_ = Math.min(minValue_, centroidMeans_[0]);
     maxValue_ = Math.max(maxValue_, centroidMeans_[numCentroids_ - 1]);
   }
@@ -283,5 +404,9 @@ public final class TDigest {
       }
       return f.apply(x);
     }
+  }
+  
+  private static double weightedAverage(final double x1, final double w1, final double x2, final double w2) {
+    return (x1 * w1 + x2 * w2) / (w1 + w2);
   }
 }
--- src/test/java/org/apache/datasketches/tdigest/TDigestTest.java
+++ src/test/java/org/apache/datasketches/tdigest/TDigestTest.java
@@ -36,6 +36,8 @@ public class TDigestTest {
     assertEquals(td.getTotalWeight(), 0);
     assertThrows(IllegalArgumentException.class, () -> td.getMinValue());
     assertThrows(IllegalArgumentException.class, () -> td.getMaxValue());
+    assertThrows(IllegalArgumentException.class, () -> td.getRank(0));
+    assertThrows(IllegalArgumentException.class, () -> td.getQuantile(0.5));
   }
 
   @Test
@@ -47,6 +49,12 @@ public class TDigestTest {
     assertEquals(td.getTotalWeight(), 1);
     assertEquals(td.getMinValue(), 1);
     assertEquals(td.getMaxValue(), 1);
+    assertEquals(td.getRank(0.99), 0);
+    assertEquals(td.getRank(1), 0.5);
+    assertEquals(td.getRank(1.01), 1);
+    assertEquals(td.getQuantile(0), 1);
+    assertEquals(td.getQuantile(0.5), 1);
+    assertEquals(td.getQuantile(1), 1);
   }
 
   @Test
@@ -54,13 +62,53 @@ public class TDigestTest {
     final TDigest td = new TDigest(100);
     final int n = 10000;
     for (int i = 0; i < n; i++) td.update(i);
-    System.out.println(td.toString(true));
-    td.compress();
-    System.out.println(td.toString(true));
+//    System.out.println(td.toString(true));
+//    td.compress();
+//    System.out.println(td.toString(true));
     assertFalse(td.isEmpty());
     assertEquals(td.getTotalWeight(), n);
     assertEquals(td.getMinValue(), 0);
     assertEquals(td.getMaxValue(), n - 1);
+    assertEquals(td.getRank(0), 0, 0.0001);
+    assertEquals(td.getRank(n / 4), 0.25, 0.0001);
+    assertEquals(td.getRank(n / 2), 0.5, 0.0001);
+    assertEquals(td.getRank(n * 3 / 4), 0.75, 0.0001);
+    assertEquals(td.getRank(n), 1);
+    assertEquals(td.getQuantile(0), 0);
+    assertEquals(td.getQuantile(0.5), n / 2, n / 2 * 0.01);
+    assertEquals(td.getQuantile(0.9), n * 0.9, n * 0.9 * 0.01);
+    assertEquals(td.getQuantile(0.95), n * 0.95, n * 0.95 * 0.01);
+    assertEquals(td.getQuantile(1), n - 1);
+  }
+
+  @Test
+  public void mergeSmall() {
+    final TDigest td1 = new TDigest(100);
+    td1.update(1);
+    td1.update(2);
+    final TDigest td2 = new TDigest(100);
+    td2.update(2);
+    td2.update(3);
+    td1.merge(td2);
+    assertEquals(td1.getTotalWeight(), 4);
+    assertEquals(td1.getMinValue(), 1);
+    assertEquals(td1.getMaxValue(), 3);
+  }
+
+  @Test
+  public void mergeLarge() {
+    final int n = 10000;
+    final TDigest td1 = new TDigest(100);
+    final TDigest td2 = new TDigest(100);
+    for (int i = 0; i < n / 2; i++) {
+      td1.update(i);
+      td2.update(n / 2 + i);
+    }
+    td1.merge(td2);
+    assertEquals(td1.getTotalWeight(), n);
+    assertEquals(td1.getMinValue(), 0);
+    assertEquals(td1.getMaxValue(), n - 1);
+//    System.out.println(td1.toString(true));
   }
 
 }
