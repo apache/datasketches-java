9b89c912fa84b1ebd9a38cedaaf5dba9c5edd203
jmalkin <786705+jmalkin@users.noreply.github.com>
2024-04-08 16:28:34 -0700

    use preferred private static order

--- src/test/java/org/apache/datasketches/filters/bloomfilter/DirectBitArrayTest.java
+++ src/test/java/org/apache/datasketches/filters/bloomfilter/DirectBitArrayTest.java
@@ -30,7 +30,7 @@ import org.testng.annotations.Test;
 
 public class DirectBitArrayTest {
 
-  static private WritableMemory bitArrayToWritableMemory(HeapBitArray ba) {
+  private static WritableMemory bitArrayToWritableMemory(HeapBitArray ba) {
     // assumes we're using small enough an array to test that
     // size can be measured with an int
     final int numBytes = (int) ba.getSerializedSizeBytes();
